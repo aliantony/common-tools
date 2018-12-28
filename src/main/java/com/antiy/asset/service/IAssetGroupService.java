70998bf3e828e8c258ef036fe73f7380d4b576ab
YangZihan <709976999@qq.com>
2018-12-28 14:37:20 +0800

    feature: 生产模板代码

---
+++ src/main/java/com/antiy/asset/service/IAssetGroupService.java
@@ -0,0 +1,52 @@
+package com.antiy.asset.service;
+
+import com.antiy.common.base.IBaseService;
+import com.antiy.common.base.PageResult;
+import java.io.Serializable;
+import java.util.List;
+
+import com.antiy.asset.entity.vo.query.AssetGroupQuery;
+import com.antiy.asset.entity.vo.request.AssetGroupRequest;
+import com.antiy.asset.entity.vo.response.AssetGroupResponse;
+import com.antiy.asset.entity.AssetGroup;
+
+
+/**
+ * <p>
+ * 资产组表 服务类
+ * </p>
+ *
+ * @author yangzihan
+ * @since 2018-12-28
+ */
+public interface IAssetGroupService extends IBaseService<AssetGroup> {
+
+        /**
+         * 保存
+         * @param assetGroupRequest
+         * @return
+         */
+        Integer saveAssetGroup(AssetGroupRequest assetGroupRequest) throws Exception;
+
+        /**
+         * 修改
+         * @param assetGroupRequest
+         * @return
+         */
+        Integer updateAssetGroup(AssetGroupRequest assetGroupRequest) throws Exception;
+
+        /**
+         * 查询对象集合
+         * @param assetGroupQuery
+         * @return
+         */
+        List<AssetGroupResponse> findListAssetGroup(AssetGroupQuery assetGroupQuery) throws Exception;
+
+        /**
+         * 批量查询
+         * @param assetGroupQuery
+         * @return
+         */
+        PageResult<AssetGroupResponse> findPageAssetGroup(AssetGroupQuery assetGroupQuery) throws Exception;
+
+}
---
+++ src/main/java/com/antiy/asset/service/impl/AssetDepartmentServiceImpl.java
@@ -0,0 +1,64 @@
+package com.antiy.asset.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import com.antiy.common.base.BaseServiceImpl;
+import com.antiy.common.base.PageResult;
+import com.antiy.common.base.BaseConverter;
+import org.springframework.stereotype.Service;
+
+import com.antiy.asset.entity.AssetDepartment;
+import com.antiy.asset.dao.AssetDepartmentDao;
+import com.antiy.asset.service.IAssetDepartmentService;
+import com.antiy.asset.entity.vo.request.AssetDepartmentRequest;
+import com.antiy.asset.entity.vo.response.AssetDepartmentResponse;
+import com.antiy.asset.entity.vo.query.AssetDepartmentQuery;
+
+import javax.annotation.Resource;
+import java.util.List;
+/**
+ * <p>
+ * 资产部门信息 服务实现类
+ * </p>
+ *
+ * @author yangzihan
+ * @since 2018-12-28
+ */
+@Service
+@Slf4j
+public class AssetDepartmentServiceImpl extends BaseServiceImpl<AssetDepartment> implements IAssetDepartmentService{
+
+
+        @Resource
+        private AssetDepartmentDao assetDepartmentDao;
+
+        private BaseConverter<AssetDepartmentRequest, AssetDepartment>  requestConverter;
+        
+        private BaseConverter<AssetDepartment, AssetDepartmentResponse> responseConverter;
+
+        @Override
+        public Integer saveAssetDepartment(AssetDepartmentRequest request) throws Exception {
+            AssetDepartment assetDepartment = requestConverter.convert(request, AssetDepartment.class);
+            return assetDepartmentDao.insert(assetDepartment);
+        }
+
+        @Override
+        public Integer updateAssetDepartment(AssetDepartmentRequest request) throws Exception {
+            AssetDepartment assetDepartment = requestConverter.convert(request, AssetDepartment.class);
+            return assetDepartmentDao.update(assetDepartment);
+        }
+
+        @Override
+        public List<AssetDepartmentResponse> findListAssetDepartment(AssetDepartmentQuery query) throws Exception {
+            return assetDepartmentDao.findListAssetDepartment(query);
+        }
+
+        public Integer findCountAssetDepartment(AssetDepartmentQuery query) throws Exception {
+            return assetDepartmentDao.findCount(query);
+        }
+
+        @Override
+        public PageResult<AssetDepartmentResponse> findPageAssetDepartment(AssetDepartmentQuery query) throws Exception {
+            return new PageResult<>(this.findCountAssetDepartment(query),this.findListAssetDepartment(query));
+        }
+}
