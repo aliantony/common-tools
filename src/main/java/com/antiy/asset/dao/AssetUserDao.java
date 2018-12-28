ab624280b399e81883dfdf3334eea373cdad86f7
hhbeast <cBa@1!YgHzrL0OeW>
2018-12-28 13:43:36 +0800

    chore： 补提dao

---
+++ src/main/java/com/antiy/asset/dao/AssetUserDao.java
@@ -0,0 +1,20 @@
+package com.antiy.asset.dao;
+
+import java.util.List;
+import com.antiy.common.base.IBaseDao;
+import com.antiy.asset.entity.AssetUser;
+import com.antiy.asset.entity.vo.query.AssetUserQuery;
+import com.antiy.asset.entity.vo.response.AssetUserResponse;
+
+/**
+ * <p>
+ * 资产用户信息 Mapper 接口
+ * </p>
+ *
+ * @author zhangyajun
+ * @since 2018-12-28
+ */
+public interface AssetUserDao extends IBaseDao<AssetUser> {
+
+    List<AssetUserResponse> findListAssetUser(AssetUserQuery query) throws Exception;
+}
