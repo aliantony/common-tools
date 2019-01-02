bfad9c9083d7bcb0a8759a6404d899e88b158cdd
zhangyajun <alikaifa@aliyun.com>
2019-01-02 16:39:48 +0800

    chore： 优化导入包

--- src/main/java/com/antiy/asset/service/impl/AssetSafetyEquipmentServiceImpl.java
+++ src/main/java/com/antiy/asset/service/impl/AssetSafetyEquipmentServiceImpl.java
@@ -1,22 +1,16 @@
 package com.antiy.asset.service.impl;
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import java.util.List;
+import com.antiy.asset.dao.AssetSafetyEquipmentDao;
+import com.antiy.asset.entity.AssetSafetyEquipment;
+import com.antiy.asset.service.IAssetSafetyEquipmentService;
+import com.antiy.asset.vo.query.AssetSafetyEquipmentQuery;
+import com.antiy.asset.vo.request.AssetSafetyEquipmentRequest;
+import com.antiy.asset.vo.response.AssetSafetyEquipmentResponse;
+import com.antiy.common.base.BaseConverter;
 import com.antiy.common.base.BaseServiceImpl;
 import com.antiy.common.base.PageResult;
-import com.antiy.common.base.BaseConverter;
-import java.util.ArrayList;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import com.antiy.asset.entity.AssetSafetyEquipment;
-import com.antiy.asset.dao.AssetSafetyEquipmentDao;
-import com.antiy.asset.service.IAssetSafetyEquipmentService;
-import com.antiy.asset.entity.dto.AssetSafetyEquipment;
-import com.antiy.asset.entity.vo.request.AssetSafetyEquipmentRequest;
-import com.antiy.asset.entity.vo.response.AssetSafetyEquipmentResponse;
-import com.antiy.asset.entity.vo.query.AssetSafetyEquipmentQuery;
-
 
 import javax.annotation.Resource;
 import java.util.List;
