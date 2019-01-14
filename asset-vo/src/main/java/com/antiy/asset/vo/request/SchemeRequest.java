81a02e95fa1115a986ba0b2610b223fc36f6ed1c
zhangyajun <zhangyajun@antiy.cn>
2019-01-14 15:08:57 +0800

    chore: 修正字段名（assetd->assetId）

--- asset-vo/src/main/java/com/antiy/asset/vo/request/SchemeRequest.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/request/SchemeRequest.java
@@ -21,7 +21,7 @@ public class SchemeRequest extends BasicRequest implements ObjectValidator {
      * 资产主键
      */
     @ApiModelProperty("资产主键")
-    private Integer assetd;
+    private Integer assetId;
 
     /**
      * 类型（1.准入实施、2.效果检查、3.制定待退役方案、4.验证退役方案、5.实施退役方案）
@@ -66,12 +66,12 @@ public class SchemeRequest extends BasicRequest implements ObjectValidator {
     @ApiModelProperty("目标状态")
     private Integer targetStatus;
 
-    public Integer getAssetd() {
-        return assetd;
+    public Integer getAssetId() {
+        return assetId;
     }
 
-    public void setAssetd(Integer assetd) {
-        this.assetd = assetd;
+    public void setAssetId(Integer assetId) {
+        this.assetId = assetId;
     }
 
     public Integer getType() {
