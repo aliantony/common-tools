b9bc8c373c83a732d0976f2909d087a25b8830de
YangZihan <709976999@qq.com>
2019-01-15 16:30:41 +0800

    feat: 添加模板封装类

--- asset-web/src/main/java/com/antiy/asset/templet/AssetPeripheralEquipmentEntity.java
+++ asset-web/src/main/java/com/antiy/asset/templet/AssetPeripheralEquipmentEntity.java
@@ -2,53 +2,36 @@ package com.antiy.asset.templet;
 
 import com.antiy.asset.annotation.ExcelField;
 
-public class SafetyEquipment {
+/**
+ * 外设
+ */
+public class AssetPeripheralEquipmentEntity {
     /**
      * 资产名称
      */
-    @ExcelField(value = "name", align = 1, title = "资产名称", type = 0)
+    @ExcelField(value = "name", align = 1, title = "资产名称")
     private String name;
     /**
      * 品类
      */
-    @ExcelField(value = "category_model", align = 1, title = "资产品类", type = 0)
+    @ExcelField(value = "category_model", align = 1, title = "资产品类")
     private String categoryModel;
 
     /**
-     * 编号
+     * 资产型号
      */
-    @ExcelField(value = "number", align = 1, title = "编号", type = 0)
+    @ExcelField(value = "number", align = 1, title = "编号")
     private String number;
     /**
      * 序列号
      */
-    @ExcelField(value = "serial", align = 1, title = "序列号", type = 0)
+    @ExcelField(value = "serial", align = 1, title = "序列号")
     private String serial;
     /**
      * 厂商
      */
-    @ExcelField(value = "manufacturer", align = 1, title = "厂商", type = 0)
+    @ExcelField(value = "manufacturer", align = 1, title = "厂商")
     private String manufacturer;
-    /**
-     * ip地址
-     */
-    @ExcelField(value = "ip", align = 1, title = "ip地址", type = 0)
-    private String ip;
-    /**
-     * 固件版本
-     */
-    @ExcelField(value = "firmware_version", align = 1, title = "固件版本", type = 0)
-    private String firmwareVersion;
-    /**
-     * 物理位置
-     */
-    @ExcelField(value = "location", align = 1, title = "物理位置")
-    private String location;
-    /**
-     * 所属分组
-     */
-    @ExcelField(value = "asset_group", align = 1, title = "所属分组")
-    private String assetGroup;
     /**
      * 使用者
      */
@@ -60,9 +43,9 @@ public class SafetyEquipment {
     @ExcelField(value = "telephone", align = 1, title = "联系电话")
     private String telephone;
     /**
-     * 使用者邮箱
+     * 邮箱
      */
-    @ExcelField(value = "email", align = 1, title = "使用者邮箱")
+    @ExcelField(value = "email", align = 1, title = "邮箱")
     private String email;
     /**
      * 资产来源
@@ -108,38 +91,6 @@ public class SafetyEquipment {
 
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public String getFirmwareVersion() {
-        return firmwareVersion;
-    }
-
-    public void setFirmwareVersion(String firmwareVersion) {
-        this.firmwareVersion = firmwareVersion;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getAssetGroup() {
-        return assetGroup;
-    }
-
-    public void setAssetGroup(String assetGroup) {
-        this.assetGroup = assetGroup;
     }
 
     public String getUser() {
