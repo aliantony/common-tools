9a8c8804152e1ca34121eea726b83e5dda871d3e
zhangyajun <zhangyajun@antiy.cn>
2019-01-17 11:51:14 +0800

    chore: 抽取代码

---
+++ asset-vo/src/main/java/com/antiy/asset/vo/enums/SoftwareStatusJumpEnum.java
@@ -0,0 +1,68 @@
+package com.antiy.asset.vo.enums;
+
+/**
+ * 资产状态跃迁
+ *
+ * @author zhangyajun
+ * @create 2019-01-15 16:13
+ **/
+public enum SoftwareStatusJumpEnum {
+                                 WATI_REGSIST(1, 3,
+                                              2), WAIT_SETTING(3, 4,
+                                                               3), WAIT_VALIDATE(4, 5,
+                                                                                 3), WAIT_NET(5, 6,
+                                                                                              3), WAIT_CHECK(6, 7,
+                                                                                                             5), WAIT_RETIRE(8,
+                                                                                                                             9,
+                                                                                                                             8);
+    // code
+    private Integer currentStatus;
+
+    // msg
+    private Integer agreeStatus;
+
+    private Integer refuseStatus;
+
+    SoftwareStatusJumpEnum(Integer currentStatus, Integer agreeStatus, Integer refuseStatus) {
+        this.currentStatus = currentStatus;
+        this.agreeStatus = agreeStatus;
+        this.refuseStatus = refuseStatus;
+    }
+
+    public static Integer getNextStatus(Integer currentStatus, Integer isAgree) {
+        for (SoftwareStatusJumpEnum statusJumpEnum : SoftwareStatusJumpEnum.values()) {
+            if (statusJumpEnum.currentStatus.equals(currentStatus)) {
+                if (isAgree.equals(ProcessTypeEnum.YES.getCode())) {
+                    return statusJumpEnum.getAgreeStatus();
+                } else {
+                    return statusJumpEnum.getRefuseStatus();
+                }
+            }
+        }
+        return -1;
+    }
+
+    public Integer getCurrentStatus() {
+        return currentStatus;
+    }
+
+    public void setCurrentStatus(Integer currentStatus) {
+        this.currentStatus = currentStatus;
+    }
+
+    public Integer getAgreeStatus() {
+        return agreeStatus;
+    }
+
+    public void setAgreeStatus(Integer agreeStatus) {
+        this.agreeStatus = agreeStatus;
+    }
+
+    public Integer getRefuseStatus() {
+        return refuseStatus;
+    }
+
+    public void setRefuseStatus(Integer refuseStatus) {
+        this.refuseStatus = refuseStatus;
+    }
+}
---
+++ asset-web/src/main/java/com/antiy/asset/service/Processor.java
@@ -0,0 +1,16 @@
+package com.antiy.asset.service;
+
+import com.antiy.asset.entity.AssetOperationRecord;
+import com.antiy.asset.vo.request.SchemeRequest;
+
+/**
+ * @author zhangyajun
+ * @create 2019-01-16 17:41
+ **/
+public interface Processor {
+    /**
+     * 保存流程操作历史
+     * @param record
+     */
+    void saveOperationHistory(SchemeRequest schemeRequest, AssetOperationRecord record) throws Exception;
+}
