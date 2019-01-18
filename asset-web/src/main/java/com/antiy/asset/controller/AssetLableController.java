961ea582189f40a32acf69f92d749c098ab3858c
YangZihan <709976999@qq.com>
2019-01-18 14:07:16 +0800

    feat: 添加了权限注解

--- asset-web/src/main/java/com/antiy/asset/controller/AssetLableController.java
+++ asset-web/src/main/java/com/antiy/asset/controller/AssetLableController.java
@@ -2,6 +2,7 @@ package com.antiy.asset.controller;
 
 import javax.annotation.Resource;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,6 +38,7 @@ public class AssetLableController {
     @ApiOperation(value = "保存接口", notes = "传入实体对象信息")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"), })
     @RequestMapping(value = "/save/single", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('asset:label:saveSingle')")
     public ActionResponse saveSingle(@RequestBody(required = false) @ApiParam(value = "assetLable") AssetLableRequest assetLable) throws Exception {
         iAssetLableService.saveAssetLable(assetLable);
         return ActionResponse.success();
@@ -51,6 +53,7 @@ public class AssetLableController {
     @ApiOperation(value = "修改接口", notes = "传入实体对象信息")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"), })
     @RequestMapping(value = "/update/single", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('asset:label:updateSingle')")
     public ActionResponse updateSingle(@RequestBody(required = false) @ApiParam(value = "assetLable") AssetLableRequest assetLable) throws Exception {
         iAssetLableService.updateAssetLable(assetLable);
         return ActionResponse.success();
@@ -65,6 +68,7 @@ public class AssetLableController {
     @ApiOperation(value = "批量查询接口", notes = "传入查询条件")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"), })
     @RequestMapping(value = "/query/list", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('asset:label:queryList')")
     public ActionResponse queryList(@ApiParam(value = "assetLable") AssetLableQuery assetLable) throws Exception {
         return ActionResponse.success(iAssetLableService.findPageAssetLable(assetLable));
     }
@@ -78,6 +82,7 @@ public class AssetLableController {
     @ApiOperation(value = "通过ID查询", notes = "主键封装对象")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"), })
     @RequestMapping(value = "/query/id", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('asset:label:queryById')")
     public ActionResponse queryById(@ApiParam(value = "assetLable") QueryCondition query) throws Exception {
         ParamterExceptionUtils.isBlank(query.getPrimaryKey(), "ID不能为空");
         return ActionResponse.success(iAssetLableService.getById(Integer.parseInt(query.getPrimaryKey())));
@@ -92,6 +97,7 @@ public class AssetLableController {
     @ApiOperation(value = "通过ID删除接口", notes = "主键封装对象")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"), })
     @RequestMapping(value = "/delete/id", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('asset:label:deleteById')")
     public ActionResponse deleteById(@ApiParam(value = "query") QueryCondition query) throws Exception {
         ParamterExceptionUtils.isBlank(query.getPrimaryKey(), "ID不能为空");
         return ActionResponse.success(iAssetLableService.deleteById(Integer.parseInt(query.getPrimaryKey())));
