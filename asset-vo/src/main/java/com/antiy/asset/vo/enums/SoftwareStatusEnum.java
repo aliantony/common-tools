c94d99a332e7104f0c451c697f2ab9e6fddb8d72
zhangyajun <zhangyajun@antiy.cn>
2019-01-19 10:21:46 +0800

    chore: 新建软件状态枚举

--- asset-vo/src/main/java/com/antiy/asset/vo/enums/SoftwareStatusEnum.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/enums/SoftwareStatusEnum.java
@@ -3,19 +3,16 @@ package com.antiy.asset.vo.enums;
 import org.apache.commons.lang.StringUtils;
 
 /**
- * @Auther: zhangbing
- * @Date: 2019/1/3 13:27
+ * @Auther: zhangyajun
+ * @Date: 2019/1/19 10:21
  * @Description:
  */
 public enum SoftwareStatusEnum {
     WATI_REGSIST(1, "待登记"),
     NOT_REGSIST(2, "不予登记"),
-    WAIT_SETTING(3, "待配置"),
-    WAIT_VALIDATE(4, "待验证"),
-    WAIT_NET(5, "待入网"),
-    NET_IN(6, "已入网"),
-    WAIT_RETIRE(7, "待退役"),
-    RETIRE(8, "已退役");
+    WAIT_ANALYZE(3, "待分析"),
+    ALLOW_INSTALL(4, "可安装"),
+    RETIRE(5, "已退役");
 
     SoftwareStatusEnum(Integer code, String msg) {
         this.code = code;
