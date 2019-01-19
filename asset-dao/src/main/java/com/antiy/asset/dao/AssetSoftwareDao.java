9396b5236b4c0110e5cca6660af09d7ca5c385cd
zhangyajun <zhangyajun@antiy.cn>
2019-01-19 09:35:10 +0800

    chore: 删除弃用方法

--- asset-dao/src/main/java/com/antiy/asset/dao/AssetSoftwareDao.java
+++ asset-dao/src/main/java/com/antiy/asset/dao/AssetSoftwareDao.java
@@ -10,9 +10,7 @@ import com.antiy.asset.vo.query.AssetSoftwareQuery;
 import com.antiy.common.base.IBaseDao;
 
 /**
- * <p>
- * 软件信息表 Mapper 接口
- * </p>
+ * <p> 软件信息表 Mapper 接口 </p>
  *
  * @author zhangyajun
  * @since 2019-01-02
@@ -24,18 +22,13 @@ public interface AssetSoftwareDao extends IBaseDao<AssetSoftware> {
     int checkRepeatSoftware(AssetSoftware software);
 
     /**
-     * 查询下拉项的资产操作系统信息
-     *
-     * @return
-     */
-    List<String> findOS() throws Exception;
-
-    /**
      * 模糊查询所有厂商
      * @param manufacturerName
      * @return
      */
-    List<String> findManufacturerName(@Param(value = "manufacturerName") String manufacturerName,@Param("areaIds") List<Integer> areaIds);
+    List<String> findManufacturerName(@Param(value = "manufacturerName") String manufacturerName,
+                                      @Param("areaIds") List<Integer> areaIds);
+
     /**
      * 统计厂商数量
      *
