d08c2d604c0adfc4b57836adc2c939122f5b2675
zhangyajun <zhangyajun@antiy.cn>
2019-01-22 20:02:25 +0800

    chore: 新增文件（资产状态跃迁静态工厂类）

---
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetStatusChangeFactory.java
@@ -0,0 +1,23 @@
+package com.antiy.asset.service.impl;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.antiy.common.utils.SpringUtil;
+
+/**
+ * 资产状态跃迁静态工厂类
+ * @author zhangyajun
+ * @create 2019-01-22 18:55
+ **/
+public class AssetStatusChangeFactory {
+    public static <T extends AbstractAssetStatusChangeProcessImpl> T createHuman(Class<T> c) {
+        AbstractAssetStatusChangeProcessImpl statusChangeProcess = null;
+        try {
+            statusChangeProcess = (AbstractAssetStatusChangeProcessImpl) SpringUtil
+                .getBean(StringUtils.uncapitalize(c.getSimpleName()));
+        } catch (Exception e) {
+            // 异常处理
+        }
+        return (T) statusChangeProcess;
+    }
+}
