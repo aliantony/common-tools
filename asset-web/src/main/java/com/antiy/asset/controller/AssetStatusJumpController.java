c16d412728eeba1410d0db7e79bd06b855534f21
zhangyajun <zhangyajun@antiy.cn>
2019-01-22 20:02:42 +0800

    chore: 新增文件（资产状态跃迁统一接口）

---
+++ asset-web/src/main/java/com/antiy/asset/controller/AssetStatusJumpController.java
@@ -0,0 +1,50 @@
+package com.antiy.asset.controller;
+
+import org.slf4j.Logger;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.antiy.asset.service.impl.AssetStatusChangeFactory;
+import com.antiy.asset.service.impl.AssetStatusChangeProcessImpl;
+import com.antiy.asset.service.impl.SoftWareStatusChangeProcessImpl;
+import com.antiy.asset.vo.request.AssetStatusReqeust;
+import com.antiy.common.base.ActionResponse;
+import com.antiy.common.utils.LogUtils;
+
+import io.swagger.annotations.*;
+
+/**
+ * 资产状态跃迁统一接口
+ *
+ * @author zhangyajun
+ * @since 2019-01-04
+ */
+@Api(value = "statusjump", description = "资产状态跃迁")
+@RestController
+@RequestMapping("/api/v1/asset/statusjump")
+public class AssetStatusJumpController {
+    private static final Logger logger = LogUtils.get();
+
+    /**
+     * 资产状态跃迁
+     *
+     * @param assetStatusReqeust
+     * @return actionResponse
+     */
+    @ApiOperation(value = "硬件状态跃迁", notes = "传入实体对象信息")
+    @PreAuthorize("hasAuthority('asset:scheme:saveSingle')")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = String.class, responseContainer = "actionResponse"), })
+    @RequestMapping(value = "/hardware", method = RequestMethod.POST)
+    public ActionResponse hardwareStatusJump(@ApiParam(value = "assetStatusReqeust") @RequestBody(required = false) AssetStatusReqeust assetStatusReqeust) throws Exception {
+        if (assetStatusReqeust.getSoftware()) {
+            return AssetStatusChangeFactory.createHuman(AssetStatusChangeProcessImpl.class)
+                .changeStatus(assetStatusReqeust);
+        } else {
+            return AssetStatusChangeFactory.createHuman(SoftWareStatusChangeProcessImpl.class)
+                .changeStatus(assetStatusReqeust);
+        }
+    }
+}
