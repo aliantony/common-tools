ac5ba5305537c5ddb42a62bbe5b9632b84542c1f
bolobeach <bolobeach@gmail.com>
2019-01-23 10:22:50 +0800

    feat:新增日志记录枚举

--- asset-vo/src/main/java/com/antiy/asset/vo/enums/AssetEventEnum.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/enums/AssetEventEnum.java
@@ -11,7 +11,17 @@ public enum AssetEventEnum {
     ASSET_STATUS_CHANGE(3,"硬件资产状态修改"),
     ASSET_CATEGORY_DELETE(4,"品类型号删除"),
     ASSET_CATEGORY_INSERT(5,"品类型号新增"),
-    ASSET_CATEGORY_UPDATE(6,"品类型号修改");
+    ASSET_CATEGORY_UPDATE(6,"品类型号修改"),
+    ASSET_DELETE(7,"硬件资产删除"),
+    ASSET_EXPORT_COMPUTER(8,"导入计算设备"),
+    ASSET_EXPORT_NET(9,"导入网络设备"),
+    ASSET_EXPORT_OTHERS(10,"导入其他设备"),
+    ASSET_EXPORT_SAFETY(11,"导入安全设备"),
+    ASSET_EXPORT_STORAGE(12,"导入存储设备"),
+    ASSET_START_ACTIVITY(13,"启动流程"),
+    ASSET_CPU_INSERT(14,"硬件资产CPU新增"),
+    ASSET_CPU_UPDATE(15,"硬件资产CPU修改"),
+    ASSET_CPU_DELETE(16,"硬件资产CPU删除");
 
 
     private Integer status;
