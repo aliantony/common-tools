dc0ad2269e698f8f89668147e9588af74682a9fc
bolobeach <bolobeach@gmail.com>
2019-01-23 10:29:18 +0800

    feat:新增日志记录枚举

--- asset-vo/src/main/java/com/antiy/asset/vo/enums/AssetEventEnum.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/enums/AssetEventEnum.java
@@ -21,7 +21,23 @@ public enum AssetEventEnum {
     ASSET_START_ACTIVITY(13,"启动流程"),
     ASSET_CPU_INSERT(14,"硬件资产CPU新增"),
     ASSET_CPU_UPDATE(15,"硬件资产CPU修改"),
-    ASSET_CPU_DELETE(16,"硬件资产CPU删除");
+    ASSET_CPU_DELETE(16,"硬件资产CPU删除"),
+    ASSET_DEPARTMENT_INSERT(17,"资产部门新增"),
+    ASSET_DEPAETMENT_UPDATE(18,"资产部门修改"),
+    ASSET_DEPAETMENT_DELETE(19,"资产部门删除"),
+    ASSET_GROUP_DELETE(20,"资产组删除"),
+    ASSET_GROUP_INSERT(21,"资产组新增"),
+    ASSET_GROUP_UPDATE(22,"资产组修改"),
+    ASSET_GROUP_RELATION_UPDATE(23,"资产组关联修改"),
+    ASSET_GROUP_RELATION_DELETE(24,"资产组关联删除"),
+    ASSET_GROUP_RELATION_INSERT(25,"资产组关联新增"),
+    ASSET_DISK_INSERT(26,"资产硬盘新增"),
+    ASSET_DISK_UPDATE(27,"资产硬盘修改"),
+    ASSET_DISK_DELETE(28,"资产硬盘删除"),
+    ASSET_MAINBORAD_DELETE(29,"资产主板删除"),
+    ASSET_MAINBORAD_UPDATE(30,"资产主板修改"),
+    ASSET_MAINBORAD_INSERT(31,"资产主板新增"),
+    ;
 
 
     private Integer status;
