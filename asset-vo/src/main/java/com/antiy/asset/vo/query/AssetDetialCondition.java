b4acb675384208c3792ed9cf2f06c3ee2022bc1d
lvliang <lvliang@antiy.cn>
2019-01-26 13:59:36 +0800

    feat: 资产实体修改

---
+++ asset-vo/src/main/java/com/antiy/asset/vo/query/AssetDetialCondition.java
@@ -0,0 +1,78 @@
+package com.antiy.asset.vo.query;
+
+import com.antiy.common.base.QueryCondition;
+
+public class AssetDetialCondition extends QueryCondition {
+    /**
+     * 是否需要查询cpu,1:是，0否
+     */
+    private Integer isNeedCpu;
+    /**
+     * 是否需要查询网卡,1:是，0否
+     */
+    private Integer isNeedNetwork;
+    /**
+     * 是否需要查询内存,1:是，0否
+     */
+    private Integer isNeedMemory;
+    /**
+     * 是否需要查询主板,1:是，0否
+     */
+    private Integer isNeedMainboard;
+    /**
+     * 是否需要查询硬盘,1:是，0否
+     */
+    private Integer isNeedHarddisk;
+    /**
+     * 是否需要查询cpu,1:是，0否
+     */
+    private Integer isNeedSoftware;
+
+    public Integer getIsNeedCpu() {
+        return isNeedCpu;
+    }
+
+    public void setIsNeedCpu(Integer isNeedCpu) {
+        this.isNeedCpu = isNeedCpu;
+    }
+
+    public Integer getIsNeedNetwork() {
+        return isNeedNetwork;
+    }
+
+    public void setIsNeedNetwork(Integer isNeedNetwork) {
+        this.isNeedNetwork = isNeedNetwork;
+    }
+
+    public Integer getIsNeedMemory() {
+        return isNeedMemory;
+    }
+
+    public void setIsNeedMemory(Integer isNeedMemory) {
+        this.isNeedMemory = isNeedMemory;
+    }
+
+    public Integer getIsNeedMainboard() {
+        return isNeedMainboard;
+    }
+
+    public void setIsNeedMainboard(Integer isNeedMainboard) {
+        this.isNeedMainboard = isNeedMainboard;
+    }
+
+    public Integer getIsNeedHarddisk() {
+        return isNeedHarddisk;
+    }
+
+    public void setIsNeedHarddisk(Integer isNeedHarddisk) {
+        this.isNeedHarddisk = isNeedHarddisk;
+    }
+
+    public Integer getIsNeedSoftware() {
+        return isNeedSoftware;
+    }
+
+    public void setIsNeedSoftware(Integer isNeedSoftware) {
+        this.isNeedSoftware = isNeedSoftware;
+    }
+}
