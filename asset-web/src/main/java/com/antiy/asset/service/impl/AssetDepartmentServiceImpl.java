ac32867c1b8dd1907936317ac52558d0b0a58b55
YangZihan <709976999@qq.com>
2019-01-30 17:54:32 +0800

    feat: 修改了mapper sql语句不正确的问题，新增部门更新的条件判断

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetDepartmentServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetDepartmentServiceImpl.java
@@ -86,8 +86,12 @@ public class AssetDepartmentServiceImpl extends BaseServiceImpl<AssetDepartment>
     @Override
     public ActionResponse updateAssetDepartment(AssetDepartmentRequest request) throws Exception {
         AssetDepartment assetDepartment = requestConverter.convert(request, AssetDepartment.class);
+        AssetDepartment parent = assetDepartmentDao.getById(Integer.parseInt(assetDepartment.getParentId()));
         if (checkNameRepeat(request)) {
             return ActionResponse.fail(RespBasicCode.BUSSINESS_EXCETION, "该部门名已存在");
+        }
+        if (Objects.isNull(parent)) {
+            return ActionResponse.fail(RespBasicCode.BUSSINESS_EXCETION, "父级部门不存在");
         }
         assetDepartment.setStatus(1);
         assetDepartment.setGmtModified(System.currentTimeMillis());
