00f2ab56ae9ade6ffb60ce9c08f89a60b70b8c9a
zhangyajun <zhangyajun@antiy.cn>
2019-02-19 10:21:34 +0800

    chore: 新增类型选项（6.硬件基准验证）

--- asset-vo/src/main/java/com/antiy/asset/vo/request/SchemeRequest.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/request/SchemeRequest.java
@@ -24,7 +24,7 @@ public class SchemeRequest extends BasicRequest implements ObjectValidator {
     /**
      * 类型（1.准入实施、2.效果检查、3.制定待退役方案、4.验证退役方案、5.实施退役方案）
      */
-    @ApiModelProperty("类型（1.准入实施、2.效果检查、3.制定待退役方案、4.实施退役方案、5.资产变更）")
+    @ApiModelProperty("类型（1.准入实施、2.效果检查、3.制定待退役方案、4.实施退役方案、5.资产变更, 6.硬件基准验证）")
     @NotNull(message = "类型不能为空")
     private Integer type;
 
