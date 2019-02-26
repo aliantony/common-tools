fe320e0783a0472ddb60396116272b39918e0375
zhangyajun <zhangyajun@antiy.cn>
2019-02-26 11:49:38 +0800

    chore: 根据新的产品新需求，添加软件待退役状态

--- asset-vo/src/main/java/com/antiy/asset/vo/enums/SoftwareStatusJumpEnum.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/enums/SoftwareStatusJumpEnum.java
@@ -9,7 +9,8 @@ package com.antiy.asset.vo.enums;
 public enum SoftwareStatusJumpEnum {
     WATI_REGSIST(SoftwareStatusEnum.WATI_REGSIST, SoftwareStatusEnum.WAIT_ANALYZE, SoftwareStatusEnum.NOT_REGSIST),
     WAIT_ANALYZE(SoftwareStatusEnum.WAIT_ANALYZE, SoftwareStatusEnum.ALLOW_INSTALL,SoftwareStatusEnum.WATI_REGSIST),
-    ALLOW_INSTALL(SoftwareStatusEnum.ALLOW_INSTALL, SoftwareStatusEnum.RETIRE, SoftwareStatusEnum.ALLOW_INSTALL);
+    ALLOW_INSTALL(SoftwareStatusEnum.ALLOW_INSTALL, SoftwareStatusEnum.WAIT_RETIRE, SoftwareStatusEnum.ALLOW_INSTALL),
+    WAIT_RETIRE(SoftwareStatusEnum.WAIT_RETIRE, SoftwareStatusEnum.RETIRE, SoftwareStatusEnum.WAIT_RETIRE);
 
     // code
     private SoftwareStatusEnum currentStatus;
