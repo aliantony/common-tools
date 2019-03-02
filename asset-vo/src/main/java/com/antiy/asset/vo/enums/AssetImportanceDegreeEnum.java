d5517cca4d8b0664d6aaa6d3b0c786c789f8a7d9
zhangyajun <zhangyajun@antiy.cn>
2019-03-02 17:00:15 +0800

    fixed: 汉化查询数据

---
+++ asset-vo/src/main/java/com/antiy/asset/vo/enums/AssetImportanceDegreeEnum.java
@@ -0,0 +1,35 @@
+package com.antiy.asset.vo.enums;
+
+/**
+ * 重要程度
+ *
+ * @Auther: zhangyajun
+ * @Date: 2019/1/14 11:36
+ * @Description:
+ */
+public enum AssetImportanceDegreeEnum implements CodeEnum {
+    CORE(1, "核心"),
+    IMPORTANCE(2, "重要"),
+    COMMON(3, "一般");
+
+    AssetImportanceDegreeEnum(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    // code
+    private Integer code;
+
+    // msg
+    private String msg;
+
+    @Override
+    public Integer getCode() {
+        return code;
+    }
+    @Override
+    public String getMsg() {
+        return msg;
+    }
+
+}
