568a25801b5b825d79d574c7090b214dfd26c968
bolobeach <bolobeach@gmail.com>
2019-03-13 17:03:18 +0800

    feat:修改无法登记资产

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
@@ -501,7 +501,7 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
         StringBuilder stringBuilder = new StringBuilder();
         assetGroup.forEach(assetGroupRequest -> {
             try {
-                String assetGroupName = assetGroupDao.getById(assetGroupRequest.getId())
+                String assetGroupName = assetGroupDao.getById(DataTypeUtils.stringToInteger(assetGroupRequest.getId()))
                     .getName();
                 asset.setAssetGroup(
                     stringBuilder.append(assetGroupName).append(",").substring(0, stringBuilder.length() - 1));
