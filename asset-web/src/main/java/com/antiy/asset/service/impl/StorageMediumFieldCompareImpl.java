b7645de05bf18bf8ebbb9f3cd08ef8eec0bf4997
zhangyajun <zhangyajun@antiy.cn>
2019-03-18 14:37:59 +0800

    feat: 存储设备变更字段比对

--- asset-web/src/main/java/com/antiy/asset/service/impl/StorageMediumFieldCompareImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/StorageMediumFieldCompareImpl.java
@@ -10,17 +10,18 @@ import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import com.antiy.asset.entity.Asset;
+import com.antiy.asset.entity.AssetStorageMedium;
 import com.antiy.asset.util.CompareUtils;
 import com.antiy.asset.util.DataTypeUtils;
 import com.antiy.asset.vo.enums.InfoLabelEnum;
 import com.antiy.asset.vo.request.AssetOuterRequest;
 import com.antiy.asset.vo.request.AssetRequest;
 import com.antiy.asset.vo.request.AssetStorageMediumRequest;
-import com.antiy.asset.vo.request.SysArea;
 import com.antiy.biz.util.RedisKeyUtil;
 import com.antiy.biz.util.RedisUtil;
 import com.antiy.common.base.BaseConverter;
 import com.antiy.common.base.BaseRequest;
+import com.antiy.common.base.SysArea;
 import com.antiy.common.base.SysUser;
 import com.antiy.common.enums.ModuleEnum;
 import com.antiy.common.utils.JsonUtil;
@@ -40,17 +41,17 @@ public class StorageMediumFieldCompareImpl extends AbstractChangeRecordCompareIm
     @Override
     List<Map<String, Object>> compareCommonBusinessInfo(Integer businessId) throws Exception {
         Integer hardware = 1;
-        Integer oldInfo = 1;
-        Integer newInfo = 0;
+        int old = 0;
+        int fresh = 1;
         List<String> changeValStrList = super.getTwoRecentChangeVal(businessId, hardware);
         List<Map<String, Object>> changeValList = new ArrayList<>();
         if (CollectionUtils.isNotEmpty(changeValStrList) && changeValStrList.size() > 1) {
             // 变更前的信息
-            AssetOuterRequest oldAssetOuterRequest = JsonUtil.json2Object(changeValStrList.get(oldInfo),
+            AssetOuterRequest oldAssetOuterRequest = JsonUtil.json2Object(changeValStrList.get(old),
                 AssetOuterRequest.class);
             // 变更后的信息
             Asset oldAsset = assetRequestToAssetConverter.convert(oldAssetOuterRequest.getAsset(), Asset.class);
-            AssetOuterRequest newAssetOuterRequest = JsonUtil.json2Object(changeValStrList.get(newInfo),
+            AssetOuterRequest newAssetOuterRequest = JsonUtil.json2Object(changeValStrList.get(fresh),
                 AssetOuterRequest.class);
             Asset newAsset = assetRequestToAssetConverter.convert(newAssetOuterRequest.getAsset(), Asset.class);
             // 拆分资产信息为通用信息和业务信息，便于前端显示
@@ -71,16 +72,15 @@ public class StorageMediumFieldCompareImpl extends AbstractChangeRecordCompareIm
             // 业务信息
             Asset oldAssetBusinessInfo = new Asset();
             // redis调用（通过区域ID查询名称）
-            String oldAreaKey = RedisKeyUtil.getKeyWhenGetObject(ModuleEnum.SYSTEM.getType(), SysArea.class,
-                DataTypeUtils.stringToInteger(newAsset.getAreaId()));
-            SysArea oldSysArea = redisUtil.getObject(oldAreaKey, SysArea.class);
-            if(oldSysArea != null) {
-                oldAssetBusinessInfo.setAreaName(oldSysArea.getFullName());
-            }
-            String oldKey = RedisKeyUtil.getKeyWhenGetObject(ModuleEnum.SYSTEM.getType(), SysUser.class,
+            String oldAreaKey = RedisKeyUtil.getKeyWhenGetObject(ModuleEnum.SYSTEM.getType(),
+                com.antiy.common.base.SysArea.class, DataTypeUtils.stringToInteger(newAsset.getAreaId()));
+            com.antiy.common.base.SysArea oldSysArea = redisUtil.getObject(oldAreaKey,
+                com.antiy.common.base.SysArea.class);
+            oldAssetBusinessInfo.setAreaName(oldSysArea.getFullName());
+            String key = RedisKeyUtil.getKeyWhenGetObject(ModuleEnum.SYSTEM.getType(), SysUser.class,
                 DataTypeUtils.stringToInteger(newAsset.getResponsibleUserId()));
-            SysUser oldSysUser = redisUtil.getObject(oldKey, SysUser.class);
-            oldAssetBusinessInfo.setResponsibleUserName(oldSysUser == null ? null : oldSysUser.getName());
+            SysUser sysUser = redisUtil.getObject(key, SysUser.class);
+            oldAssetBusinessInfo.setResponsibleUserName(sysUser == null ? "" : sysUser.getName());
             oldAssetBusinessInfo.setContactTel(oldAsset.getContactTel());
             oldAssetBusinessInfo.setEmail(oldAsset.getEmail());
             oldAssetBusinessInfo.setAssetGroup(oldAsset.getAssetGroup());
@@ -88,17 +88,16 @@ public class StorageMediumFieldCompareImpl extends AbstractChangeRecordCompareIm
             oldAssetBusinessInfo.setLocation(oldAsset.getLocation());
             oldAssetBusinessInfo.setHouseLocation(oldAsset.getHouseLocation());
             oldAssetBusinessInfo.setDescrible(oldAsset.getDescrible());
+            oldAssetBusinessInfo.setBuyDate(oldAsset.getBuyDate());
+            oldAssetBusinessInfo.setWarranty(newAsset.getWarranty());
 
             Asset newAssetBusinessInfo = new Asset();
             // redis调用（通过区域ID查询名称）
-            String newAreaKey = RedisKeyUtil.getKeyWhenGetObject(ModuleEnum.SYSTEM.getType(), SysArea.class,
-                DataTypeUtils.stringToInteger(newAsset.getAreaId()));
-            SysArea newSysArea = redisUtil.getObject(newAreaKey, SysArea.class);
-            newAssetBusinessInfo.setAreaName(newSysArea.getFullName());
-            String newKey = RedisKeyUtil.getKeyWhenGetObject(ModuleEnum.SYSTEM.getType(), SysUser.class,
-                DataTypeUtils.stringToInteger(newAsset.getResponsibleUserId()));
-            SysUser newSysUser = redisUtil.getObject(newKey, SysUser.class);
-            newAssetBusinessInfo.setResponsibleUserName(newSysUser == null ? null : newSysUser.getName());
+            String newAreaKey = RedisKeyUtil.getKeyWhenGetObject(ModuleEnum.SYSTEM.getType(),
+                com.antiy.common.base.SysArea.class, DataTypeUtils.stringToInteger(newAsset.getAreaId()));
+            com.antiy.common.base.SysArea newSysArea = redisUtil.getObject(newAreaKey, SysArea.class);
+            oldAssetBusinessInfo.setAreaName(newSysArea.getFullName());
+
             newAssetBusinessInfo.setContactTel(newAsset.getContactTel());
             newAssetBusinessInfo.setEmail(newAsset.getEmail());
             newAssetBusinessInfo.setAssetGroup(newAsset.getAssetGroup());
@@ -106,21 +105,55 @@ public class StorageMediumFieldCompareImpl extends AbstractChangeRecordCompareIm
             newAssetBusinessInfo.setLocation(newAsset.getLocation());
             newAssetBusinessInfo.setHouseLocation(newAsset.getHouseLocation());
             newAssetBusinessInfo.setDescrible(newAsset.getDescrible());
+            newAssetBusinessInfo.setBuyDate(newAsset.getBuyDate());
+            newAssetBusinessInfo.setServiceLife(newAsset.getServiceLife());
+            newAssetBusinessInfo.setWarranty(newAsset.getWarranty());
 
             List<Map<String, Object>> assetBusinessInfoCompareResult = CompareUtils.compareClass(oldAssetBusinessInfo,
                 newAssetBusinessInfo, InfoLabelEnum.BUSINESSINFO.getMsg());
-            List<Map<String, Object>> storageEquipmentCompareResult = null;
-            AssetStorageMediumRequest oldStorageMediumRequest = oldAssetOuterRequest.getAssetStorageMedium();
-            AssetStorageMediumRequest newStorageMediumRequest = newAssetOuterRequest.getAssetStorageMedium();
-            if (newStorageMediumRequest != null && oldStorageMediumRequest != null) {
-                storageEquipmentCompareResult = CompareUtils.compareClass(oldStorageMediumRequest,
-                    newStorageMediumRequest, InfoLabelEnum.BUSINESSINFO.getMsg());
-                changeValList.addAll(storageEquipmentCompareResult);
-            }
+
+            // 组合数据
             changeValList.addAll(assetCommonInoCompareResult);
             changeValList.addAll(assetBusinessInfoCompareResult);
+            changeValList.addAll(getDetailCompareResult(oldAssetOuterRequest, newAssetOuterRequest));
         }
         return changeValList;
+    }
+
+    /**
+     * 获取详细信息字段的对比结果
+     * @param oldAssetOuterRequest
+     * @throws ClassNotFoundException
+     * @throws IllegalAccessException
+     */
+    private List<Map<String, Object>> getDetailCompareResult(AssetOuterRequest oldAssetOuterRequest,
+                                                             AssetOuterRequest newAssetOuterRequest) throws IllegalAccessException {
+        AssetStorageMediumRequest oldStorageMediumRequest = oldAssetOuterRequest.getAssetStorageMedium();
+        AssetStorageMediumRequest newStorageMediumRequest = newAssetOuterRequest.getAssetStorageMedium();
+        AssetStorageMedium oldStorageMedium = new AssetStorageMedium();
+
+        oldStorageMedium.setMaximumStorage(oldStorageMediumRequest.getMaximumStorage());
+        oldStorageMedium.setDiskNumber(oldStorageMediumRequest.getDiskNumber());
+        oldStorageMedium.setHighCache(oldStorageMediumRequest.getHighCache());
+        oldStorageMedium.setInnerInterface(oldStorageMediumRequest.getInnerInterface());
+        oldStorageMedium.setRaidSupport(oldStorageMediumRequest.getRaidSupport());
+        oldStorageMedium.setAverageTransferRate(oldStorageMediumRequest.getAverageTransferRate());
+        oldStorageMedium.setFirmware(oldStorageMediumRequest.getFirmware());
+        oldStorageMedium.setOsVersion(oldStorageMediumRequest.getOsVersion());
+        oldStorageMedium.setDriverNumber(oldStorageMediumRequest.getDriverNumber());
+
+        AssetStorageMedium newStorageMedium = new AssetStorageMedium();
+        newStorageMedium.setMaximumStorage(newStorageMediumRequest.getMaximumStorage());
+        newStorageMedium.setDiskNumber(newStorageMediumRequest.getDiskNumber());
+        newStorageMedium.setHighCache(newStorageMediumRequest.getHighCache());
+        newStorageMedium.setInnerInterface(newStorageMediumRequest.getInnerInterface());
+        newStorageMedium.setRaidSupport(newStorageMediumRequest.getRaidSupport());
+        newStorageMedium.setAverageTransferRate(newStorageMediumRequest.getAverageTransferRate());
+        newStorageMedium.setFirmware(newStorageMediumRequest.getFirmware());
+        newStorageMedium.setOsVersion(newStorageMediumRequest.getOsVersion());
+        newStorageMedium.setDriverNumber(newStorageMediumRequest.getDriverNumber());
+
+        return CompareUtils.compareClass(oldStorageMedium, newStorageMedium, InfoLabelEnum.BUSINESSINFO.getMsg());
     }
 
     @Override
