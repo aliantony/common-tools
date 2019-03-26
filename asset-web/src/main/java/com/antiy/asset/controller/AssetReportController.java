0e1d7ea70bdfd5add78c8cd0490bb76edfaa21a5
zhangyajun <zhangyajun@antiy.cn>
2019-03-26 09:45:15 +0800

    feat: 资产报表controller

---
+++ asset-web/src/main/java/com/antiy/asset/controller/AssetReportController.java
@@ -0,0 +1,17 @@
+package com.antiy.asset.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+/**
+ * @author zhangyajun
+ * @since 2019-01-02
+ */
+@Api(value = "AssetReport", description = "资产报表")
+@RestController
+@RequestMapping("/api/v1/asset/report")
+public class AssetReportController {
+
+}
