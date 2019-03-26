5c2dba2cb1cac7840752813263a8973a8fc748da
why <wanghongyu@antiy.cn>
2019-03-26 20:19:12 +0800

    feat:硬件

---
+++ asset-dao/src/main/java/com/antiy/asset/entity/AssetGroupEntity.java
@@ -0,0 +1,47 @@
+package com.antiy.asset.entity;
+
+/**
+ * 报表统计结果
+ */
+public class AssetGroupEntity {
+    // 统计数
+    private Integer groupCount;
+
+    private Integer groupId;
+    // 名字
+    private Integer name;
+    // 日期
+    private String  date;
+
+    public Integer getGroupCount() {
+        return groupCount;
+    }
+
+    public void setGroupCount(Integer groupCount) {
+        this.groupCount = groupCount;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public Integer getName() {
+        return name;
+    }
+
+    public void setName(Integer name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
