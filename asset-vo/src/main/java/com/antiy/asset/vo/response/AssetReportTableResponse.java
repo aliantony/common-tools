76c71e2a7605d1cf1e76065ad9513cb8fb6ff465
zhangyajun <zhangyajun@antiy.cn>
2019-03-28 15:15:43 +0800

    refactor: 修改字段类型

--- asset-vo/src/main/java/com/antiy/asset/vo/response/AssetReportTableResponse.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/response/AssetReportTableResponse.java
@@ -1,6 +1,7 @@
 package com.antiy.asset.vo.response;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p> AssetReportResponse 资产报表表格返回对象 </p>
@@ -9,9 +10,9 @@ import java.util.List;
  * @since 2019-03-27
  */
 public class AssetReportTableResponse extends BaseResponse {
-    private String                formName;
-    private List<ReportTableHead> children;
-    private List<String>          rows;
+    private String                    formName;
+    private List<ReportTableHead>     children;
+    private List<Map<String, String>> rows;
 
     public String getFormName() {
         return formName;
@@ -25,15 +26,15 @@ public class AssetReportTableResponse extends BaseResponse {
         return children;
     }
 
-    public void setChildren(List<ReportTableHead> children) {
-        this.children = children;
-    }
-
-    public List<String> getRows() {
+    public List<Map<String, String>> getRows() {
         return rows;
     }
 
-    public void setRows(List<String> rows) {
+    public void setRows(List<Map<String, String>> rows) {
         this.rows = rows;
+    }
+
+    public void setChildren(List<ReportTableHead> children) {
+        this.children = children;
     }
 }
