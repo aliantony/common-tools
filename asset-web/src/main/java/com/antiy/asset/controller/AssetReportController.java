319e043aed102380e0e6f0a43dbcb20bef4eb365
zhangyajun <zhangyajun@antiy.cn>
2019-03-28 16:51:29 +0800

    feat: 新增接口（返回表格数据）

--- asset-web/src/main/java/com/antiy/asset/controller/AssetReportController.java
+++ asset-web/src/main/java/com/antiy/asset/controller/AssetReportController.java
@@ -1,19 +1,22 @@
 package com.antiy.asset.controller;
 
-import com.antiy.asset.service.IAssetAreaReportService;
-import com.antiy.asset.service.IAssetReportService;
-import com.antiy.asset.vo.query.AssetReportCategoryCountQuery;
-import com.antiy.asset.vo.request.ReportQueryRequest;
-import com.antiy.asset.vo.response.AssetReportResponse;
-import com.antiy.common.base.ActionResponse;
-import io.swagger.annotations.*;
+import javax.annotation.Resource;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+import com.antiy.asset.service.IAssetAreaReportService;
+import com.antiy.asset.service.IAssetReportService;
+import com.antiy.asset.vo.query.AssetReportCategoryCountQuery;
+import com.antiy.asset.vo.request.ReportQueryRequest;
+import com.antiy.asset.vo.response.AssetReportResponse;
+import com.antiy.asset.vo.response.AssetReportTableResponse;
+import com.antiy.common.base.ActionResponse;
+
+import io.swagger.annotations.*;
 
 /**
  * @author zhangyajun
@@ -41,6 +44,19 @@ public class AssetReportController {
     // @PreAuthorize("hasAuthority('asset:report:categoryAmountByTime')")
     public ActionResponse queryCategoryCountByTime(AssetReportCategoryCountQuery query) throws Exception {
         return ActionResponse.success(iAssetReportService.queryCategoryCountByTime(query));
+    }
+
+    /**
+     * 根据时间条件查询分类统计资产数量,返回表格数据
+     *
+     * @return actionResponse
+     */
+    @ApiOperation(value = "根据时间条件查询分类统计资产数量（表格数据）", notes = "主键封装对象")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = AssetReportTableResponse.class, responseContainer = "actionResponse"), })
+    @RequestMapping(value = "/query/categoryCountByTimeToTable", method = RequestMethod.GET)
+    // @PreAuthorize("hasAuthority('asset:report:categoryCountByTimeToTable')")
+    public ActionResponse queryCategoryCountByTimeToTable(AssetReportCategoryCountQuery query) throws Exception {
+        return ActionResponse.success(iAssetReportService.queryCategoryCountByTimeToTable(query));
     }
 
     /**
