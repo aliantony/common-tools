18c37c256f9d661c41ac5d94581db74b8b680dd1
lvliang <lvliang@antiy.cn>
2019-04-04 17:12:58 +0800

    feat: 总数统计

---
+++ asset-vo/src/main/java/com/antiy/asset/vo/enums/ConfigureStatusEnum.java
@@ -0,0 +1,32 @@
+package com.antiy.asset.vo.enums;
+
+/**
+ * 配置状态
+ * @author lvliang
+ */
+public enum ConfigureStatusEnum {
+                                 NOCONFIGURE(1, "未配置"), CONFIGURING(2, "配置中"), CONFIGURED(3, "已配置");
+    private Integer code;
+    private String  name;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    ConfigureStatusEnum(Integer code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
