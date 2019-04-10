ff1b199215a95b759474a938ce1831017b331e0a
zhangyajun <zhangyajun@antiy.cn>
2019-04-10 11:32:25 +0800

    fixed: 删除重复的日志声明

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
@@ -144,8 +144,6 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
     private ActivityClient                                                     activityClient;
     @Resource
     private AreaClient                                                         areaClient;
-    private static final Logger                                                logger   = LogUtils
-        .get(AssetServiceImpl.class);
     @Resource
     private AesEncoder                                                         aesEncoder;
     @Resource
@@ -2851,6 +2849,7 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
         }
     }
 
+    @Override
     public List<String> pulldownUnconnectedManufacturer() throws Exception {
         AssetQuery query = new AssetQuery();
         Map<String, String> categoryMap = assetCategoryModelService.getSecondCategoryMap();
