e22f7462b4ae9a103ddca61d666e6eaa3df26a9f
why <wanghongyu@antiy.cn>
2019-04-11 11:17:58 +0800

    feat:更新硬件必填

--- asset-vo/src/main/java/com/antiy/asset/vo/request/AssetMemoryRequest.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/request/AssetMemoryRequest.java
@@ -6,7 +6,6 @@ import com.antiy.common.exception.RequestParamValidateException;
 import com.antiy.common.validation.ObjectValidator;
 import io.swagger.annotations.ApiModelProperty;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -31,22 +30,22 @@ public class AssetMemoryRequest extends BasicRequest implements ObjectValidator 
     @Encode
     private String  assetId;
     /**
-     * 硬盘品牌
+     * 内存品牌
      */
     @ApiModelProperty("内存品牌")
-    @NotBlank(message = "内存品牌不能为空")
+//    @NotBlank(message = "内存品牌不能为空")
     @Size(message = "内存品牌长度不能超过64位", max = 64)
     private String  brand;
     /**
      * 序列号
      */
     @ApiModelProperty("序列号")
-    @Size(message = "主板序列号长度不能超过32位",max = 32)
+    @Size(message = "序列号长度不能超过32位",max = 32)
     private String serial;
     /**
      * 内存类型：1未知，2-ddr2,3-ddr3,4-ddr4
      */
-    @NotNull(message = "内存类型不能为空")
+//    @NotNull(message = "内存类型不能为空")
     @ApiModelProperty("内存类型：1未知，2-ddr2,3-ddr3,4-ddr4")
     private Integer transferType;
     /**
@@ -59,7 +58,7 @@ public class AssetMemoryRequest extends BasicRequest implements ObjectValidator 
      * 内存主频(MHz)
      */
     @ApiModelProperty("内存主频(MHz)")
-    @NotNull(message = "内存主频不能为空")
+//    @NotNull(message = "内存主频不能为空")
     private Double  frequency;
     /**
      * 插槽类型:1-SDRAM,2-SIMM,3-DIMM,4-RIMM
