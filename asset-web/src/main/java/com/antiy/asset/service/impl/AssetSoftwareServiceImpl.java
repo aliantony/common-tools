1fda52057c28976028268a1860d1ad2899142574
zhangyajun <zhangyajun@antiy.cn>
2019-04-11 10:00:49 +0800

    refactor: 添加备注信息

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetSoftwareServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetSoftwareServiceImpl.java
@@ -759,6 +759,7 @@ public class AssetSoftwareServiceImpl extends BaseServiceImpl<AssetSoftware> imp
     private Scheme convertScheme(ConfigRegisterRequest registerRequest) {
         Scheme scheme = new Scheme();
         scheme.setContent(registerRequest.getSuggest());
+        scheme.setMemo(registerRequest.getSuggest());
         if (AssetTypeEnum.SOFTWARE.getCode().equals(DataTypeUtils.stringToInteger(registerRequest.getSource()))) {
             scheme.setAssetNextStatus(SoftwareStatusEnum.ALLOW_INSTALL.getCode());
             scheme.setSchemeSource(2);
