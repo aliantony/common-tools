587b84b7c5ef256c9c0ee7e6aa7e93b04ce8a3c7
lvliang <lvliang@antiy.cn>
2019-04-12 17:09:35 +0800

    feat: 总数统计

--- asset-web/src/main/java/com/antiy/asset/intergration/impl/EmergencyClientImpl.java
+++ asset-web/src/main/java/com/antiy/asset/intergration/impl/EmergencyClientImpl.java
@@ -1,12 +1,14 @@
 package com.antiy.asset.intergration.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.antiy.asset.aop.AssetLog;
 import com.antiy.asset.entity.IdCount;
 import com.antiy.asset.intergration.EmergencyClient;
 import com.antiy.asset.util.BaseClient;
 import com.antiy.common.base.ActionResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -16,7 +18,7 @@ import java.util.List;
  * 资产告警数量获取
  * @author lvliang
  */
-@Service
+@Component
 public class EmergencyClientImpl implements EmergencyClient {
     @Value("${emergencyClientUrl}")
     private String     emergencyClientUrl;
@@ -25,6 +27,7 @@ public class EmergencyClientImpl implements EmergencyClient {
     private BaseClient baseClient;
 
     @Override
+    @AssetLog(description = "远程调用获取资产告警数量")
     public ActionResponse<List<IdCount>> queryEmergencyCount(List<Integer> assetIds) {
         return (ActionResponse) baseClient.post(assetIds, new ParameterizedTypeReference<ActionResponse>() {
         }, emergencyClientUrl);
