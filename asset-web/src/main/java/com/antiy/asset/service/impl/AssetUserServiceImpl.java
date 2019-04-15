5cf9130ebe7677b0a9bcb3a3a504f496db1ab229
zhangyajun <zhangyajun@antiy.cn>
2019-04-15 11:07:07 +0800

    fixed:  an incompatible @Transactional

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetUserServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetUserServiceImpl.java
@@ -81,6 +81,7 @@ public class AssetUserServiceImpl extends BaseServiceImpl<AssetUser> implements 
     }
 
     @Override
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public List<AssetUserResponse> findListAssetUser(AssetUserQuery query) throws Exception {
         List<AssetUser> assetUser = assetUserDao.queryUserList(query);
         if (CollectionUtils.isNotEmpty(assetUser)) {
@@ -98,6 +99,7 @@ public class AssetUserServiceImpl extends BaseServiceImpl<AssetUser> implements 
         return convert(assetUser);
     }
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public Integer findCountAssetUser(AssetUserQuery query) throws Exception {
         return assetUserDao.findCount(query);
     }
