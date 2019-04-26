2818aca27b2514c570d4738170893b4f7f79f157
zhangyajun <zhangyajun@antiy.cn>
2019-04-26 17:30:58 +0800

    feat: 获取操作人员信息 接口定义

---
+++ asset-web/src/main/java/com/antiy/asset/intergration/SysUserClient.java
@@ -0,0 +1,15 @@
+package com.antiy.asset.intergration;
+
+import com.antiy.common.base.ActionResponse;
+
+/**
+ * 区域查询接口
+ * @author zhangyajun
+ * @create 2019-02-21 15:47
+ **/
+public interface SysUserClient {
+
+    ActionResponse queryUserRoleById(String id);
+
+    Object getInvokeResult(String id);
+}
