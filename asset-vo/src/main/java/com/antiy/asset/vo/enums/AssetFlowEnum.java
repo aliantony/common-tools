9bea3d05d7f1e61b253e212bc4fcd1e1f3c0b566
zhangyajun <zhangyajun@antiy.cn>
2019-04-28 12:05:45 +0800

    refactor: 硬件操作记录描述

--- asset-vo/src/main/java/com/antiy/asset/vo/enums/AssetFlowEnum.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/enums/AssetFlowEnum.java
@@ -8,14 +8,11 @@ package com.antiy.asset.vo.enums;
  * @Description:
  */
 public enum AssetFlowEnum implements CodeEnum {
-    HARDWARE_REGISTER(AssetStatusEnum.WATI_REGSIST.getCode(),"硬件资产登记"),
-    HARDWARE_CONFIG_BASELINE(AssetStatusEnum.WAIT_SETTING.getCode(),"硬件配置基准"),
-    HARDWARE_BASELINE_VALIDATE(AssetStatusEnum.WAIT_VALIDATE.getCode(),"硬件基准验证"),
-    HARDWARE_ENTER_IMPLEMENTATION(AssetStatusEnum.WAIT_NET.getCode(),"硬件准入实施"),
-    HARDWARE_EFFECT_CHECK(AssetStatusEnum.WAIT_CHECK.getCode(),"硬件效果检查"),
+                                               HARDWARE_REGISTER(AssetStatusEnum.WATI_REGSIST
+                                                   .getCode(), "已登记资产"), HARDWARE_CONFIG_BASELINE(AssetStatusEnum.WAIT_SETTING.getCode(), "已配置基准"), HARDWARE_BASELINE_VALIDATE(AssetStatusEnum.WAIT_VALIDATE.getCode(), "已验证基准"), HARDWARE_ENTER_IMPLEMENTATION(AssetStatusEnum.WAIT_NET.getCode(), "已入网实施资产"), HARDWARE_EFFECT_CHECK(AssetStatusEnum.WAIT_CHECK.getCode(), "已入网检查资产"),
     TOPOLOGY_REGISTER(99,"拓扑登记"),
-    WAIT_RETIRE_SCHEMA(AssetStatusEnum.NET_IN.getCode(),"制定待退役方案"),
-    VALIDATE_RETIRE_RESULT(AssetStatusEnum.WAIT_RETIRE.getCode(),"实施退役");
+                                               WAIT_RETIRE_SCHEMA(AssetStatusEnum.NET_IN
+                                                   .getCode(), "已待退役资产"), VALIDATE_RETIRE_RESULT(AssetStatusEnum.WAIT_RETIRE.getCode(), "已实施退役资产");
 
     AssetFlowEnum(Integer code, String msg) {
         this.code = code;
