7be575f7a0ba343f00130375d586e56492f293fc
YangZihan <709976999@qq.com>
2019-05-16 16:37:16 +0800

    fix: 修改了校验

--- asset-vo/src/main/java/com/antiy/asset/vo/request/AssetNetworkEquipmentRequest.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/request/AssetNetworkEquipmentRequest.java
@@ -40,14 +40,14 @@ public class AssetNetworkEquipmentRequest extends BasicRequest implements Object
      * 接口数目
      */
     @ApiModelProperty("接口数目")
-    @Max(value = 9999999, message = "网口数目大小不超过9999999")
+    @Max(value = 127, message = "网口数目大小不超过127")
     private Integer interfaceSize;
     /**
      * 端口数目
      */
     @ApiModelProperty("端口数目")
     @NotNull(message = "网口数目不为空")
-    @Max(value = 9999999, message = "网口数目大小不超过9999999")
+    @Max(value = 99, message = "网口数目大小不超过99")
     private Integer portSize;
     /**
      * 是否无线:0-否,1-是
