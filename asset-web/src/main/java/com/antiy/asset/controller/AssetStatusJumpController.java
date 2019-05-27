36d9a5d6ab6ef4339e88cffe6830ba36786e3d3b
YangZihan <709976999@qq.com>
2019-05-27 17:48:35 +0800

    fix: 修改不予登记日志

--- asset-web/src/main/java/com/antiy/asset/controller/AssetStatusJumpController.java
+++ asset-web/src/main/java/com/antiy/asset/controller/AssetStatusJumpController.java
@@ -125,7 +125,7 @@ public class AssetStatusJumpController {
             operationRecord(assetStatusChangeRequest, assetId);
             LogUtils.recordOperLog(
                 new BusinessData(AssetEventEnum.SOFT_NO_REGISTER.getName(), DataTypeUtils.stringToInteger(assetId),
-                        assetStatusChangeRequest.getAssetId(), assetStatusChangeRequest, BusinessModuleEnum.SOFTWARE_ASSET,
+                    softwareDao.getById(assetId).getName(), assetStatusChangeRequest, BusinessModuleEnum.SOFTWARE_ASSET,
                     BusinessPhaseEnum.getByStatus(assetStatusChangeRequest.getStatus())));
             LogUtils.info(logger, AssetEventEnum.SOFT_UPDATE.getName() + " {}", assetStatusChangeRequest);
 
