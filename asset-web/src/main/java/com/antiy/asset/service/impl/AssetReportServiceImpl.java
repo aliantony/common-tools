af2a3b35752e3815f29abcb00479512b96f09a1a
bolobeach <bolobeach@gmail.com>
2019-05-28 15:57:21 +0800

    修改报表查询错误

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetReportServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetReportServiceImpl.java
@@ -1,5 +1,15 @@
 package com.antiy.asset.service.impl;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.collections.MapUtils;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.antiy.asset.dao.AssetCategoryModelDao;
 import com.antiy.asset.dao.AssetReportDao;
 import com.antiy.asset.entity.AssetCategoryEntity;
@@ -29,14 +39,6 @@ import com.antiy.common.exception.RequestParamValidateException;
 import com.antiy.common.utils.LogUtils;
 import com.antiy.common.utils.LoginUserUtil;
 import com.antiy.common.utils.ParamterExceptionUtils;
-import org.apache.commons.collections.MapUtils;
-import org.slf4j.Logger;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.annotation.Resource;
-import java.text.SimpleDateFormat;
-import java.util.*;
 
 /**
  * 资产报表实现类
@@ -83,8 +85,8 @@ public class AssetReportServiceImpl implements IAssetReportService {
         } else if (ShowCycleType.ASSIGN_TIME.getCode().equals(showCycleType.getCode())) {
             query.setFormat(MONTH);
             // 如果为当前月，则显示为周
-            Long mouth = ReportDateUtils.monthDiff(query.getBeginTime(),query.getEndTime());
-            if(mouth == 0){
+            Long mouth = ReportDateUtils.monthDiff(query.getBeginTime(), query.getEndTime());
+            if (mouth == 0) {
                 query.setFormat(WEEK);
             }
             map = buildCategoryCountByTime(query,
@@ -486,8 +488,9 @@ public class AssetReportServiceImpl implements IAssetReportService {
                 reportQueryRequest.setSqlTime("%Y-%m");
 
                 // 如果为当前月，则显示为周
-                Long mouth = ReportDateUtils.monthDiff(reportQueryRequest.getStartTime(),reportQueryRequest.getEndTime());
-                if(mouth == 0){
+                Long mouth = ReportDateUtils.monthDiff(reportQueryRequest.getStartTime(),
+                    reportQueryRequest.getEndTime());
+                if (mouth == 0) {
                     reportQueryRequest.setSqlTime("%U");
                 }
                 return buildGroupCountByTime(reportQueryRequest, ReportDateUtils
@@ -641,6 +644,13 @@ public class AssetReportServiceImpl implements IAssetReportService {
             return getAssetReportTableResponse(query, ReportDateUtils.getCurrentMonthOfYear());
         } else if (ShowCycleType.ASSIGN_TIME.getCode().equals(showCycleType.getCode())) {
             query.setFormat(MONTH);
+
+            // 如果为当前月，则显示为周
+            Long mouth = ReportDateUtils.monthDiff(query.getBeginTime(), query.getEndTime());
+            if (mouth == 0) {
+                query.setFormat(WEEK);
+            }
+
             return getAssetReportTableResponse(query,
                 ReportDateUtils.getMonthWithDate(query.getBeginTime(), query.getEndTime()));
         } else {
@@ -760,6 +770,13 @@ public class AssetReportServiceImpl implements IAssetReportService {
                 return buildAssetReportTable(reportQueryRequest, ReportDateUtils.getCurrentMonthOfYear(), "本年");
             case "5":
                 reportQueryRequest.setSqlTime("%Y-%m");
+                // 如果为当前月，则显示为周
+                Long mouth = ReportDateUtils.monthDiff(reportQueryRequest.getStartTime(),
+                    reportQueryRequest.getEndTime());
+                if (mouth == 0) {
+                    reportQueryRequest.setSqlTime("%U");
+                }
+
                 return buildAssetReportTable(reportQueryRequest,
                     ReportDateUtils.getMonthWithDate(reportQueryRequest.getStartTime(),
                         reportQueryRequest.getEndTime()),
