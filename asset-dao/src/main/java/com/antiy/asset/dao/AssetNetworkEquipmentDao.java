80aef2e3a4d01fa2926bd91e3d487863df90f7e6
why <wanghongyu@antiy.cn>
2019-06-06 17:58:03 +0800

    feat:修改资产

--- asset-dao/src/main/java/com/antiy/asset/dao/AssetNetworkEquipmentDao.java
+++ asset-dao/src/main/java/com/antiy/asset/dao/AssetNetworkEquipmentDao.java
@@ -1,10 +1,10 @@
 package com.antiy.asset.dao;
 
-import java.util.List;
-
 import com.antiy.asset.entity.AssetNetworkEquipment;
 import com.antiy.asset.vo.query.AssetNetworkEquipmentQuery;
 import com.antiy.common.base.IBaseDao;
+
+import java.util.List;
 
 /**
  * <p> 网络设备详情表 Mapper 接口 </p>
@@ -25,4 +25,6 @@ public interface AssetNetworkEquipmentDao extends IBaseDao<AssetNetworkEquipment
     List<AssetNetworkEquipment> findPortById(AssetNetworkEquipmentQuery query);
 
     Integer findPortAmount(String id);
+
+    Integer insertBatch(List<AssetNetworkEquipment> networkEquipments);
 }
