0a7a0d1bfb414fe1c9992f8e0bc49975a0cd2eec
lvliang <lvliang@antiy.cn>
2019-06-22 16:58:57 +0800

    feat: 资产变更

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
@@ -1440,7 +1440,7 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
         ParamterExceptionUtils.isTrue(
             !checkNumber(assetOuterRequest.getAsset().getId(), assetOuterRequest.getAsset().getNumber()), "资产编号重复");
         ParamterExceptionUtils.isTrue(
-                !checkNumber(assetOuterRequest.getAsset().getId(), assetOuterRequest.getAsset().getName()), "资产名称重复");
+                !checkName(assetOuterRequest.getAsset().getId(), assetOuterRequest.getAsset().getName()), "资产名称重复");
         Asset asset = BeanConvert.convertBean(assetOuterRequest.getAsset(), Asset.class);
         AssetQuery assetQuery = new AssetQuery();
         Long currentTimeMillis = System.currentTimeMillis();
@@ -1918,7 +1918,7 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
         return assetDao.selectRepeatNumber(number, id) > 0;
     }
     private boolean checkName(String id, String name) {
-        return assetDao.selectRepeatNumber(name, id) > 0;
+        return assetDao.selectRepeatName(name, id) > 0;
     }
 
     private void updateAssetStatus(Integer assetStatus, Long currentTimeMillis, String assetId) throws Exception {
