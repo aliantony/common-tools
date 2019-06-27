3ba7583cdb76b9b8373d6c5e3f685bc595fc3988
zhangyajun <zhangyajun@antiy.cn>
2019-06-27 13:17:36 +0800

    fixed: 解密注解

--- asset-vo/src/main/java/com/antiy/asset/vo/request/AreaIdRequest.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/request/AreaIdRequest.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import com.antiy.common.base.BaseRequest;
+import com.antiy.common.encoder.Encode;
 import com.antiy.common.exception.RequestParamValidateException;
 import com.antiy.common.validation.ObjectValidator;
 
@@ -22,6 +23,7 @@ public class AreaIdRequest extends BaseRequest implements ObjectValidator {
     @ApiModelProperty("区域ID不能为空")
     @NotNull(message = "区域ID不能为空")
     @Valid
+    @Encode
     private List<String> areaIds;
 
     public List<String> getAreaIds() {
