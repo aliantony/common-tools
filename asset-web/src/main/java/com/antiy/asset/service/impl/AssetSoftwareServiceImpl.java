13033d7c4ea193abcc83a8e072907230972f58fb
YangZihan <709976999@qq.com>
2019-07-01 16:54:20 +0800

    fix: 修改状态校验

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetSoftwareServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetSoftwareServiceImpl.java
@@ -326,7 +326,7 @@ public class AssetSoftwareServiceImpl extends BaseServiceImpl<AssetSoftware> imp
             throw new BusinessException("软件状态已改变");
         }
         if (!(Objects.equals(requestSoftwareStatusStatus, SoftwareStatusEnum.WATI_REGSIST.getCode())
-              && !(Objects.equals(requestSoftwareStatusStatus, SoftwareStatusEnum.NOT_REGSIST.getCode())))) {
+              || (Objects.equals(requestSoftwareStatusStatus, SoftwareStatusEnum.NOT_REGSIST.getCode())))) {
             throw new BusinessException("软件状态已改变");
         }
         // if (request.getActivityRequest() != null && softwareStatus.equals(SoftwareStatusEnum.RETIRE.getCode())
