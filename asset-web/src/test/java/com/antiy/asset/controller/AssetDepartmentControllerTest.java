bc12163719c3908900c35ff1ccc27ff2792e629e
chenhuasheng <1011755994@qq.com>
2019-07-09 11:20:09 +0800

    fest: 完善部分controller的UT

--- asset-web/src/test/java/com/antiy/asset/controller/AssetDepartmentControllerTest.java
+++ asset-web/src/test/java/com/antiy/asset/controller/AssetDepartmentControllerTest.java
@@ -95,9 +95,11 @@ public class AssetDepartmentControllerTest {
 
     @Test
     public void queryById()throws Exception {
+        QueryCondition queryCondition = new QueryCondition();
+        queryCondition.setPrimaryKey("1");
         MvcResult mvcResult = mockMvc.perform(
-                MockMvcRequestBuilders.get("/api/v1/asset/department/query/id").param("primaryKey","1")).andReturn();
-        Assert.assertThat(mvcResult.getResponse().getContentAsString(),containsString("{\"head\":{\"code\":\"200\",\"result\":\"成功\"},\"body\":null}"));
+                MockMvcRequestBuilders.post("/api/v1/asset/department/query/id").contentType(MediaType.APPLICATION_JSON).content(JSONObject.toJSONString(queryCondition))).andReturn();
+        Assert.assertNotNull(mvcResult);
     }
 
     @Test
@@ -135,7 +137,7 @@ public class AssetDepartmentControllerTest {
         assetDepartmentNodeResponse.setParentId("1");
         Mockito.when(iAssetDepartmentService.findDepartmentNode()).thenReturn(assetDepartmentNodeResponse);
         MvcResult mvcResult = mockMvc.perform(
-                MockMvcRequestBuilders.get("/api/v1/asset/department/query/node")
+                MockMvcRequestBuilders.post("/api/v1/asset/department/query/node")
                         .param("primaryKey","1")).andReturn();
         ActionResponse actionResponse = JsonUtil.json2Object(mvcResult.getResponse().getContentAsString(),ActionResponse.class);
         AssetDepartmentNodeResponse actual = JsonUtil.json2Object(JsonUtil.object2Json(actionResponse.getBody()),AssetDepartmentNodeResponse.class);
