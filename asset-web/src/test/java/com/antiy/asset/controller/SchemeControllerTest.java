e719dbb375f21f7532e76206468094f82bd91fe3
zhangxin <1023567017@qq.com>
2019-07-09 18:14:03 +0800

    test: 取消@SpringBootTest注解,使用PowerMockRunner

--- asset-web/src/test/java/com/antiy/asset/controller/SchemeControllerTest.java
+++ asset-web/src/test/java/com/antiy/asset/controller/SchemeControllerTest.java
@@ -35,7 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-@SpringBootTest
+//@SpringBootTest
 @RunWith(SpringRunner.class)
 public class SchemeControllerTest {
     @Mock
