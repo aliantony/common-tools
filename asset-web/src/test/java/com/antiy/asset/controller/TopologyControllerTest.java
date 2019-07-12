c6374754ca71bceea91f38e1cc98a9c435f480dd
zhangxin <1023567017@qq.com>
2019-07-12 13:06:00 +0800

    test: controller没用到，注释无用代码

--- asset-web/src/test/java/com/antiy/asset/controller/TopologyControllerTest.java
+++ asset-web/src/test/java/com/antiy/asset/controller/TopologyControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.Resource;
@@ -26,59 +28,58 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@RunWith(PowerMockRunner.class)
 public class TopologyControllerTest {
-    @MockBean
-    private ITopologyService iTopologyService;
-
-    private MockMvc mockMvc;
-
-    @Resource
-    private WebApplicationContext webApplicationContext;
-
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        this.mockMvc = webAppContextSetup(webApplicationContext).build();
-    }
-
-    @Test
-    public void testQueryTopologyInit() throws Exception {
-        when(iTopologyService.queryTopologyInit())
-                .thenReturn(Stream.of(new TopologyResponse()).collect(Collectors.toList()));
-
-        mockMvc.perform(
-                get("/api/v1/asset/topology/query/init")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .accept(MediaType.APPLICATION_JSON_UTF8))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void testSaveTopology() throws Exception {
-        when(iTopologyService.saveTopology(any())).thenReturn(0);
-
-        String json = "{\"dataJson\":\"{}\",\"topologyType\":\"PHYSICS\"}";
-
-        mockMvc.perform(
-                post("/api/v1/asset/topology/save")
-                        .accept(MediaType.APPLICATION_JSON_UTF8)
-                        .content(json)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    @Test
-    public void testQueryTopology() throws Exception {
-        when(iTopologyService.queryTopology(anyInt())).thenReturn("queryTopologyResponse");
-
-        mockMvc.perform(
-                get("/api/v1/asset/topology/query?topologyType=PHYSICS")
-                        .contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
+//    @InjectMocks
+//    private TopologyController topologyController;
+//    @MockBean
+//    private ITopologyService iTopologyService;
+//
+//    private MockMvc mockMvc;
+//
+//    @Before
+//    public void setUp() {
+//        MockitoAnnotations.initMocks(this);
+//        this.mockMvc = MockMvcBuilders.standaloneSetup(topologyController).build();
+//    }
+//
+//    @Test
+//    public void testQueryTopologyInit() throws Exception {
+//        when(iTopologyService.queryTopologyInit())
+//                .thenReturn(Stream.of(new TopologyResponse()).collect(Collectors.toList()));
+//
+//        mockMvc.perform(
+//                get("/api/v1/asset/topology/query/init")
+//                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+//                        .accept(MediaType.APPLICATION_JSON_UTF8))
+//                .andDo(print())
+//                .andExpect(status().isOk());
+//    }
+//
+//    @Test
+//    public void testSaveTopology() throws Exception {
+//        when(iTopologyService.saveTopology(any())).thenReturn(0);
+//
+//        String json = "{\"dataJson\":\"{}\",\"topologyType\":\"PHYSICS\"}";
+//
+//        mockMvc.perform(
+//                post("/api/v1/asset/topology/save")
+//                        .accept(MediaType.APPLICATION_JSON_UTF8)
+//                        .content(json)
+//                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+//                .andExpect(status().isOk())
+//                .andDo(print());
+//    }
+//
+//    @Test
+//    public void testQueryTopology() throws Exception {
+//        when(iTopologyService.queryTopology(anyInt())).thenReturn("queryTopologyResponse");
+//
+//        mockMvc.perform(
+//                get("/api/v1/asset/topology/query?topologyType=PHYSICS")
+//                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+//                .andExpect(status().isOk())
+//                .andDo(print());
+//    }
 }
