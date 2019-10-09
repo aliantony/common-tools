ba29bb0627f8098e68b513208ce971b50678ca1b
liulusheng <liulusheng@antiy.cn>
2019-10-09 13:38:34 +0800

    feat: 增加模板创建/编辑-添加软件和关联软件

--- asset-web/src/main/java/com/antiy/asset/controller/AssetHardSoftLibController.java
+++ asset-web/src/main/java/com/antiy/asset/controller/AssetHardSoftLibController.java
@@ -40,7 +40,6 @@ import javax.annotation.Resource;
 import java.util.List;
 
 /**
- *
  * @author zhangyajun
  * @since 2019-09-19
  */
@@ -59,7 +58,7 @@ public class AssetHardSoftLibController {
      * @return actionResponse
      */
     @ApiOperation(value = "保存接口", notes = "传入实体对象信息")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = Integer.class), })
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Integer.class),})
     @RequestMapping(value = "/save/single", method = RequestMethod.POST)
     public ActionResponse saveSingle(@ApiParam(value = "assetHardSoftLib") @RequestBody AssetHardSoftLibRequest assetHardSoftLibRequest) throws Exception {
         return ActionResponse.success(iAssetHardSoftLibService.saveAssetHardSoftLib(assetHardSoftLibRequest));
@@ -72,7 +71,7 @@ public class AssetHardSoftLibController {
      * @return actionResponse
      */
     @ApiOperation(value = "修改接口", notes = "传入实体对象信息")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = Integer.class), })
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Integer.class),})
     @RequestMapping(value = "/update/single", method = RequestMethod.POST)
     public ActionResponse updateSingle(@ApiParam(value = "assetHardSoftLib") AssetHardSoftLibRequest assetHardSoftLibRequest) throws Exception {
         return ActionResponse.success(iAssetHardSoftLibService.updateAssetHardSoftLib(assetHardSoftLibRequest));
@@ -85,7 +84,7 @@ public class AssetHardSoftLibController {
      * @return actionResponse
      */
     @ApiOperation(value = "批量查询接口", notes = "传入查询条件")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = AssetHardSoftLibResponse.class, responseContainer = "List"), })
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = AssetHardSoftLibResponse.class, responseContainer = "List"),})
     @RequestMapping(value = "/query/list", method = RequestMethod.POST)
     public ActionResponse queryList(@ApiParam(value = "assetHardSoftLib") @RequestBody AssetHardSoftLibQuery assetHardSoftLibQuery) throws Exception {
         return ActionResponse.success(iAssetHardSoftLibService.queryPageAssetHardSoftLib(assetHardSoftLibQuery));
@@ -98,7 +97,7 @@ public class AssetHardSoftLibController {
      * @return actionResponse
      */
     @ApiOperation(value = "通过ID查询", notes = "主键封装对象")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = AssetHardSoftLibResponse.class), })
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = AssetHardSoftLibResponse.class),})
     @RequestMapping(value = "/query/id", method = RequestMethod.GET)
     public ActionResponse queryById(@ApiParam(value = "主键封装对象") QueryCondition queryCondition) throws Exception {
         return ActionResponse.success(iAssetHardSoftLibService.queryAssetHardSoftLibById(queryCondition));
@@ -111,14 +110,14 @@ public class AssetHardSoftLibController {
      * @return actionResponse
      */
     @ApiOperation(value = "通过ID删除接口", notes = "主键封装对象")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = Integer.class), })
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = Integer.class),})
     @RequestMapping(value = "/delete/id", method = RequestMethod.POST)
     public ActionResponse deleteById(@ApiParam(value = "主键封装对象") BaseRequest baseRequest) throws Exception {
         return ActionResponse.success(iAssetHardSoftLibService.deleteAssetHardSoftLibById(baseRequest));
     }
 
     @ApiOperation(value = "分页查询资产关联的软件信息列表", notes = "必传资产ID")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = SoftwareResponse.class, responseContainer = "actionResponse") })
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = SoftwareResponse.class, responseContainer = "actionResponse")})
     @RequestMapping(value = "/software/list", method = RequestMethod.POST)
     public ActionResponse getPageSoftwarePage(@ApiParam(value = "assetId") @RequestBody AssetSoftwareQuery queryCondition) {
         return ActionResponse.success(iAssetHardSoftLibService.getPageSoftWareList(queryCondition));
@@ -130,7 +129,7 @@ public class AssetHardSoftLibController {
      * @return actionResponse
      */
     @ApiOperation(value = "操作系统下拉选项", notes = "操作系统")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = OsSelectResponse.class), })
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = OsSelectResponse.class),})
     @RequestMapping(value = "/pullDown/os", method = RequestMethod.POST)
     public ActionResponse pullDownOs(@RequestBody(required = false) OsQuery osQuery) {
         return ActionResponse.success(iAssetHardSoftLibService.pullDownOs(osQuery));
@@ -138,12 +137,12 @@ public class AssetHardSoftLibController {
 
     /**
      * 查询下拉的厂商信息
-     * @author zhangyajun
      *
      * @return 厂商名称集合
+     * @author zhangyajun
      */
     @ApiOperation(value = "查询厂商接口", notes = "无查询条件")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"), })
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"),})
     @RequestMapping(value = "/pullDown/supplier", method = RequestMethod.POST)
     @PreAuthorize(value = "hasAuthority('asset:asset:pulldownManufacturer')")
     public ActionResponse<List<String>> pulldownManufacturer(@ApiParam(value = "下拉查询类") @RequestBody AssetPulldownQuery query) throws Exception {
@@ -152,12 +151,12 @@ public class AssetHardSoftLibController {
 
     /**
      * 查询下拉的名称信息
-     * @author zhangyajun
      *
      * @return 查询下拉的名称信息
+     * @author zhangyajun
      */
     @ApiOperation(value = "查询下拉的名称信息", notes = "无查询条件")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"), })
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"),})
     @RequestMapping(value = "/pullDown/name", method = RequestMethod.POST)
     @PreAuthorize(value = "hasAuthority('asset:asset:pulldownName')")
     public ActionResponse<List<String>> pulldownName(@ApiParam(value = "下拉查询类") @RequestBody AssetPulldownQuery query) throws Exception {
@@ -168,12 +167,12 @@ public class AssetHardSoftLibController {
 
     /**
      * 查询下拉的名称信息
-     * @author zhangyajun
      *
      * @return 查询下拉的名称信息
+     * @author zhangyajun
      */
     @ApiOperation(value = "查询下拉的版本信息", notes = "无查询条件")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"), })
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = ActionResponse.class, responseContainer = "actionResponse"),})
     @RequestMapping(value = "/pullDown/version", method = RequestMethod.POST)
     @PreAuthorize(value = "hasAuthority('asset:asset:pulldownVersion')")
     public ActionResponse<List<BusinessSelectResponse>> pulldownVersion(@ApiParam(value = "下拉查询类") @RequestBody AssetPulldownQuery query) throws Exception {
@@ -182,4 +181,17 @@ public class AssetHardSoftLibController {
         ParamterExceptionUtils.isNull(query.getName(), "名称不能为空");
         return ActionResponse.success(iAssetHardSoftLibService.pulldownVersion(query));
     }
+
+    /**
+     * 软件列表分页(装机模板-添加软件）
+     *
+     * @return actionResponse
+     */
+    @ApiOperation(value = "装机模板-添加软件", notes = "软件列表")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = OsSelectResponse.class),})
+    @RequestMapping(value = "/query/softList", method = RequestMethod.POST)
+    public ActionResponse queryPageSoft(@RequestBody AssetSoftwareQuery query) {
+        return ActionResponse.success(iAssetHardSoftLibService.queryPageSoft(query));
+    }
+
 }
