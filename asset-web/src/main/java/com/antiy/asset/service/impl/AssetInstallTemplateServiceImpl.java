98c61c0b5e9a2fa68f607caeb5a0197eaacc2570
liulusheng <liulusheng@antiy.cn>
2019-10-15 14:25:22 +0800

    fix: 修改编辑的时候返回用户信息

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetInstallTemplateServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetInstallTemplateServiceImpl.java
@@ -14,9 +14,7 @@ import com.antiy.asset.vo.query.ActivityWaitingQuery;
 import com.antiy.asset.vo.query.AssetInstallTemplateQuery;
 import com.antiy.asset.vo.query.PrimaryKeyQuery;
 import com.antiy.asset.vo.request.*;
-import com.antiy.asset.vo.request.SysArea;
 import com.antiy.asset.vo.response.*;
-import com.antiy.biz.entity.SysMessageRequest;
 import com.antiy.biz.util.RedisKeyUtil;
 import com.antiy.biz.util.RedisUtil;
 import com.antiy.common.base.*;
@@ -98,7 +96,7 @@ public class AssetInstallTemplateServiceImpl extends BaseServiceImpl<AssetInstal
     @Override
     @Transactional
     public ActionResponse updateAssetInstallTemplate(AssetInstallTemplateRequest request) throws Exception {
-        boolean isUpdateStatusOnly = request.getIsUpdateStatus() == 0;
+        boolean isUpdateStatusOnly = request.getIsUpdateStatus() != null && request.getIsUpdateStatus() == 0;
         Integer templateId = request.getId();
         int currentStatus = assetInstallTemplateDao.getById(templateId.toString()).getCurrentStatus();
         int requestStatus = request.getUpdateStatus() == null ? 0 : request.getUpdateStatus();
@@ -136,10 +134,7 @@ public class AssetInstallTemplateServiceImpl extends BaseServiceImpl<AssetInstal
             return ActionResponse.fail(RespBasicCode.BUSSINESS_EXCETION, "更新状态失败");
         }
         //编辑模板
-        //todo 用户角色权限
-        // Set<SysRole> roles=  LoginUserUtil.getLoginUser().getSysRoles();//.forEach(v->System.out.println(v.getName()));
-        assetInstallTemplate.setCurrentStatus(AssetInstallTemplateStatusEnum.NOTAUDIT.getCode());
-        assetInstallTemplate.setOperationSystemName(this.queryOs(request.getOperationSystem().toString()).get(0).getOsName());
+        setTemplateInfo(request, assetInstallTemplate);
         assetInstallTemplateDao.update(assetInstallTemplate);
 
         //以前关联的软件和现在关联的软件求差集
@@ -218,10 +213,13 @@ public class AssetInstallTemplateServiceImpl extends BaseServiceImpl<AssetInstal
         AssetInstallTemplateResponse assetInstallTemplateResponse = responseConverter.convert(
                 template, AssetInstallTemplateResponse.class);
         List<AssetSysUserResponse> list = new ArrayList<>();
-        Arrays.stream(template.getExecutor().split(",")).forEach(v -> {
-            list.add(getUserNameByUserId(Integer.valueOf(v)));
-        });
-        assetInstallTemplateResponse.setExecutor(list);
+        String executor = template.getExecutor();
+        if (executor.equalsIgnoreCase("all")) {
+            assetInstallTemplateResponse.setExecutor(list);
+        } else {
+            list.add(getUserNameByUserId(Integer.valueOf(executor)));
+            assetInstallTemplateResponse.setExecutor(list);
+        }
         return assetInstallTemplateResponse;
     }
 
@@ -301,14 +299,7 @@ public class AssetInstallTemplateServiceImpl extends BaseServiceImpl<AssetInstal
         request.setGmtCreate(System.currentTimeMillis());
         AssetInstallTemplate template = requestConverter.convert(request, AssetInstallTemplate.class);
         template.setCategoryModel(1);
-        template.setCurrentStatus(AssetInstallTemplateStatusEnum.NOTAUDIT.getCode());
-        template.setOperationSystemName(this.queryOs(request.getOperationSystem().toString()).get(0).getOsName());
-        StringBuilder builders = new StringBuilder();
-        request.getNextExecutor().forEach(v -> {
-            builders.append(aesEncoder.decode(v, LoginUserUtil.getLoginUser().getUsername()));
-            builders.append(",");
-        });
-        template.setExecutor(builders.deleteCharAt(builders.length() - 1).toString());
+        setTemplateInfo(request, template);
         assetInstallTemplateDao.insert(template);
         request.setStringId(template.getStringId());
         if (request.getPatchIds() != null && !request.getPatchIds().isEmpty()) {
@@ -322,6 +313,19 @@ public class AssetInstallTemplateServiceImpl extends BaseServiceImpl<AssetInstal
                 template.getId(), request.getNumberCode(), template.toString(), BusinessModuleEnum.HARD_ASSET, BusinessPhaseEnum.NONE));
         LogUtils.info(logger, "创建装机模板:{}", template.toString());
         return sendTask(request, template);
+    }
+
+    private void setTemplateInfo(AssetInstallTemplateRequest request, AssetInstallTemplate template) {
+        template.setCurrentStatus(AssetInstallTemplateStatusEnum.NOTAUDIT.getCode());
+        template.setOperationSystemName(this.queryOs(request.getOperationSystem().toString()).get(0).getOsName());
+        List<String> executors = request.getNextExecutor().stream().collect(Collectors.toList());
+        if (executors.size() > 1) {
+            template.setExecutor("all");
+        } else if (executors.size() == 1) {
+            template.setExecutor(aesEncoder.decode(executors.get(0), LoginUserUtil.getLoginUser().getUsername()));
+        } else {
+            throw new RequestParamValidateException("请选择下一步执行人");
+        }
     }
 
     @Override
