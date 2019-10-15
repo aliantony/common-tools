0d72e8ce51720d90dd0e6098b88a3851cfa5ccc8
zhangyajun <zhangyajun@antiy.cn>
2019-10-15 11:20:20 +0800

    feat: 更新操作系统

--- asset-web/src/main/java/com/antiy/asset/service/impl/IAssetSynchRedundantImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/IAssetSynchRedundantImpl.java
@@ -37,9 +37,8 @@ public class IAssetSynchRedundantImpl implements IAssetSynchRedundant {
             throw new BusinessException("开始时间应小于结束时间");
         }
 
-        // 处理业务
+        // 更新名称、厂商、操作系统字段
         List<AssetHardSoftLib> hardSoftLibList = hardSoftLibDao.getCpeByTime(query);
-
         if (CollectionUtils.isNotEmpty(hardSoftLibList)) {
             return assetDao.updateRedundantFiled(hardSoftLibList);
         }
