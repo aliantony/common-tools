0c04483500ac3bdc07a7688611bfc36476024f3f
yangzihan <yangzihan@Antiy.cn>
2019-10-15 09:38:31 +0800

    fix: 修改查询语句

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
@@ -1036,16 +1036,6 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
         assetResponse.setDecryptId(Objects.toString(asset.getId()));
         assetResponse.setDecryptInstallTemplateId(asset.getInstallTemplateId());
         assetOuterResponse.setAsset(assetResponse);
-        // 获取厂商，名称，版本
-        AssetHardSoftLib assetHardSoftLib = assetHardSoftLibDao
-            .getByBusinessId(Objects.toString(asset.getBusinessId()));
-        assetResponse
-            .setManufacturer(Optional.ofNullable(assetHardSoftLib).map(AssetHardSoftLib::getSupplier).orElse(null));
-        assetResponse.setName(Optional.ofNullable(assetHardSoftLib).map(AssetHardSoftLib::getProductName).orElse(null));
-        String m = Optional.ofNullable(assetHardSoftLib).map(AssetHardSoftLib::getSupplier).orElse("")
-                   + Optional.ofNullable(assetHardSoftLib).map(AssetHardSoftLib::getProductName).orElse("");
-        assetResponse.setVersion(Optional.ofNullable(assetHardSoftLib).map(AssetHardSoftLib::getCpeUri)
-            .map(str -> str.substring(9 + m.length())).orElse(""));
         // 获取区域
         String key = RedisKeyUtil.getKeyWhenGetObject(ModuleEnum.SYSTEM.getType(), SysArea.class,
             DataTypeUtils.stringToInteger(asset.getAreaId()));
