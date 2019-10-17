40599b54fa0acbcf64990f2cd796ce6aba9190ab
yangzihan <yangzihan@Antiy.cn>
2019-10-17 15:36:26 +0800

    fix: 修改isNet判断

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
@@ -830,7 +830,7 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
 
         if (Objects.isNull(query.getCategoryModels()) || query.getCategoryModels().length == 0) {
             // 网络设备可关联计算设备和网络设备
-            if (query.getIsNet() == 1) {
+            if (query.getIsNet() == 2) {
                 query.setCategoryModels(
                     new Integer[] { AssetCategoryEnum.COMPUTER.getCode(), AssetCategoryEnum.NETWORK.getCode() });
             }
@@ -2963,7 +2963,7 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
     @Override
     public Set<String> pulldownUnconnectedManufacturer(Integer isNet, String primaryKey) {
         AssetQuery query = new AssetQuery();
-        if ((isNet == null) || isNet == 1) {
+        if ((isNet == null) || isNet == 2) {
             query.setCategoryModels(
                 new Integer[] { AssetCategoryEnum.COMPUTER.getCode(), AssetCategoryEnum.NETWORK.getCode() });
         } else {
