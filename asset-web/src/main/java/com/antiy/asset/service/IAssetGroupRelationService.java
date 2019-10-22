83216860fdf3b279c3e0c1300642cca62b2140cd
zhangxin <zhangxincd@antiy.cn>
2019-10-22 15:46:50 +0800

    refactor: 删除资产组关系 无用代码

--- asset-web/src/main/java/com/antiy/asset/service/IAssetGroupRelationService.java
+++ asset-web/src/main/java/com/antiy/asset/service/IAssetGroupRelationService.java
@@ -1,13 +1,7 @@
 package com.antiy.asset.service;
 
-import java.util.List;
-
 import com.antiy.asset.entity.AssetGroupRelation;
-import com.antiy.asset.vo.query.AssetGroupRelationQuery;
-import com.antiy.asset.vo.request.AssetGroupRelationRequest;
-import com.antiy.asset.vo.response.AssetGroupRelationResponse;
 import com.antiy.common.base.IBaseService;
-import com.antiy.common.base.PageResult;
 
 /**
  * <p> 资产与资产组关系表 服务类 </p>
@@ -16,60 +10,6 @@ import com.antiy.common.base.PageResult;
  * @since 2019-01-02
  */
 public interface IAssetGroupRelationService extends IBaseService<AssetGroupRelation> {
-
-    /**
-     * 保存
-     *
-     * @param request
-     * @return
-     */
-    Integer saveAssetGroupRelation(AssetGroupRelationRequest request) throws Exception;
-
-    /**
-     * 修改
-     *
-     * @param request
-     * @return
-     */
-    Integer updateAssetGroupRelation(AssetGroupRelationRequest request) throws Exception;
-
-    /**
-     * 查询对象集合
-     *
-     * @param query
-     * @return
-     */
-    List<AssetGroupRelationResponse> findListAssetGroupRelation(AssetGroupRelationQuery query) throws Exception;
-
-    /**
-     * 批量查询
-     *
-     * @param query
-     * @return
-     */
-    PageResult<AssetGroupRelationResponse> findPageAssetGroupRelation(AssetGroupRelationQuery query) throws Exception;
-
-    /**
-     * 资产组详情查询
-     *
-     * @param query
-     * @return
-     */
-    PageResult<AssetGroupRelationResponse> findPageAssetByAssetGroupId(AssetGroupRelationQuery query) throws Exception;
-
-    /**
-     * 通过资产组ID查询资产信息
-     * @param query
-     * @return
-     */
-    List<AssetGroupRelationResponse> findAssetDetailByAssetGroupId(AssetGroupRelationQuery query) throws Exception;
-
-    /**
-     * 通过资产组ID统计资产信息
-     * @param query
-     * @return
-     */
-    Integer findCountDetailByGroupId(AssetGroupRelationQuery query) throws Exception;
 
     /**
      * 资产组是否存在绑定资产
--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetGroupRelationServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetGroupRelationServiceImpl.java
@@ -1,31 +1,12 @@
 package com.antiy.asset.service.impl;
 
-import static com.antiy.biz.file.FileHelper.logger;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-import javax.annotation.Resource;
-
-import org.springframework.stereotype.Service;
-
 import com.antiy.asset.dao.AssetGroupRelationDao;
 import com.antiy.asset.entity.AssetGroupRelation;
 import com.antiy.asset.service.IAssetGroupRelationService;
-import com.antiy.asset.util.BeanConvert;
-import com.antiy.asset.util.DataTypeUtils;
-import com.antiy.asset.util.LogHandle;
-import com.antiy.asset.vo.enums.AssetEventEnum;
-import com.antiy.asset.vo.query.AssetGroupRelationDetailQuery;
-import com.antiy.asset.vo.query.AssetGroupRelationQuery;
-import com.antiy.asset.vo.request.AssetGroupRelationRequest;
-import com.antiy.asset.vo.response.AssetGroupRelationResponse;
-import com.antiy.asset.vo.response.AssetNetworkCardResponse;
 import com.antiy.common.base.BaseServiceImpl;
-import com.antiy.common.base.PageResult;
-import com.antiy.common.enums.ModuleEnum;
-import com.antiy.common.utils.LogUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p> 资产与资产组关系表 服务实现类 </p>
@@ -36,89 +17,8 @@ import com.antiy.common.utils.LogUtils;
 @Service
 public class AssetGroupRelationServiceImpl extends BaseServiceImpl<AssetGroupRelation> implements
                                                                                       IAssetGroupRelationService {
-
     @Resource
     private AssetGroupRelationDao assetGroupRelationDao;
-
-    @Override
-    public Integer saveAssetGroupRelation(AssetGroupRelationRequest request) throws Exception {
-        AssetGroupRelation assetGroupRelation = (AssetGroupRelation) BeanConvert.convert(request,
-            AssetGroupRelation.class);
-        Integer result = assetGroupRelationDao.insert(assetGroupRelation);
-        if (!Objects.equals(result, 0)) {
-            // 写入业务日志
-            LogHandle.log(assetGroupRelation.toString(), AssetEventEnum.ASSET_GROUP_RELATION_INSERT.getName(),  AssetEventEnum.ASSET_GROUP_RELATION_UPDATE.getStatus(),
-                ModuleEnum.ASSET.getCode());
-            LogUtils.info(logger, AssetEventEnum.ASSET_GROUP_RELATION_INSERT.getName() + " {}",
-                assetGroupRelation.toString());
-        }
-        return result;
-    }
-
-    @Override
-    public Integer updateAssetGroupRelation(AssetGroupRelationRequest request) throws Exception {
-        AssetGroupRelation assetGroupRelation = (AssetGroupRelation) BeanConvert.convert(request,
-            AssetGroupRelation.class);
-        Integer result = assetGroupRelationDao.update(assetGroupRelation);
-        if (!Objects.equals(0, result)) {
-            // 写入业务日志
-            LogHandle.log(assetGroupRelation.toString(), AssetEventEnum.ASSET_GROUP_RELATION_UPDATE.getName(),  AssetEventEnum.ASSET_GROUP_RELATION_UPDATE.getStatus(),
-                    ModuleEnum.ASSET.getCode());
-            LogUtils.info(logger, AssetEventEnum.ASSET_GROUP_RELATION_UPDATE.getName() + " {}",
-                    assetGroupRelation.toString());
-        }
-        return result;
-    }
-
-    @Override
-    public List<AssetGroupRelationResponse> findListAssetGroupRelation(AssetGroupRelationQuery query) throws Exception {
-        List<AssetGroupRelation> assetGroupRelation = assetGroupRelationDao.findListAssetGroupRelation(query);
-        List<AssetGroupRelationResponse> assetGroupRelationResponse = new ArrayList<>();
-        return assetGroupRelationResponse;
-    }
-
-    public Integer findCountAssetGroupRelation(AssetGroupRelationQuery query) throws Exception {
-        return assetGroupRelationDao.findCount(query);
-    }
-
-    @Override
-    public PageResult<AssetGroupRelationResponse> findPageAssetGroupRelation(AssetGroupRelationQuery query)
-                                                                                                           throws Exception {
-        return new PageResult<>(query.getPageSize(), this.findCountAssetGroupRelation(query), query.getCurrentPage(),
-            this.findListAssetGroupRelation(query));
-    }
-
-    @Override
-    public List<AssetGroupRelationResponse> findAssetDetailByAssetGroupId(AssetGroupRelationQuery query)
-                                                                                                        throws Exception {
-        AssetGroupRelationDetailQuery assetGroupRelationDetailQuery = (AssetGroupRelationDetailQuery) BeanConvert
-            .convert(query, AssetGroupRelationDetailQuery.class);
-        assetGroupRelationDetailQuery.setAssetGroupId(assetGroupRelationDetailQuery.getAssetGroupId());
-
-        List<AssetGroupRelation> assetGroupRelationList = assetGroupRelationDao.findAssetDetailByAssetGroupId(query);
-        List<AssetGroupRelationResponse> assetGroupRelationResponseList = BeanConvert.convert(assetGroupRelationList,
-            AssetGroupRelationResponse.class);
-        // for (AssetGroupRelationResponse assetGroupRelationResponse : assetGroupRelationResponseList) {
-        // List<AssetNetworkCardResponse> assetNetworkCardResponseList = BeanConvert
-        // .convert(assetNetworkCardDao.findNetworkCardByAssetId(Integer.valueOf(assetGroupRelationResponse
-        // .getStringId())), AssetNetworkCardResponse.class);
-        // assetGroupRelationResponse.setNetworkCardResponseList(assetNetworkCardResponseList);
-        // }
-
-        return assetGroupRelationResponseList;
-    }
-
-    @Override
-    public PageResult<AssetGroupRelationResponse> findPageAssetByAssetGroupId(AssetGroupRelationQuery query)
-                                                                                                            throws Exception {
-        return new PageResult<AssetGroupRelationResponse>(query.getPageSize(), this.findCountDetailByGroupId(query),
-            query.getCurrentPage(), this.findAssetDetailByAssetGroupId(query));
-    }
-
-    @Override
-    public Integer findCountDetailByGroupId(AssetGroupRelationQuery query) throws Exception {
-        return assetGroupRelationDao.findCountDetailByGroupId(DataTypeUtils.stringToInteger(query.getAssetGroupId()));
-    }
 
     @Override
     public Integer hasRealtionAsset(String primaryKey) {
