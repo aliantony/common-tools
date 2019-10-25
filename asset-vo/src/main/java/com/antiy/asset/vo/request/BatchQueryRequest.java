f4814ad91c23faf0b1e11bbb4174571a9bc5a3d5
liulusheng <liulusheng@antiy.cn>
2019-10-25 17:43:43 +0800

    feat: 增加模板删除结束工作流

--- asset-vo/src/main/java/com/antiy/asset/vo/request/BatchQueryRequest.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/request/BatchQueryRequest.java
@@ -9,28 +9,39 @@ import java.util.List;
  * 批量查询vo封装主键id列表
  */
 public class BatchQueryRequest extends BasicRequest {
-	public BatchQueryRequest() {
-	}
+    public BatchQueryRequest() {
+    }
 
-	public BatchQueryRequest(List<String> ids) {
-		this.ids = ids;
-	}
+    public BatchQueryRequest(List<String> ids) {
+        this.ids = ids;
+    }
 
-	@Encode
-	private List<String> ids;
+    @Encode
+    private List<String> ids;
+    private List<String> processInstanceIds;
 
-	public List<String> getIds() {
-		return ids;
-	}
+    public List<String> getIds() {
+        return ids;
+    }
 
-	public void setIds(List<String> ids) {
-		this.ids = ids;
-	}
+    public void setIds(List<String> ids) {
+        this.ids = ids;
+    }
 
-	@Override
-	public String toString() {
-		return "BatchQueryRequest{" +
-				"ids=" + ids +
-				'}';
-	}
+    public List<String> getProcessInstanceIds() {
+        return processInstanceIds;
+    }
+
+    public void setProcessInstanceIds(List<String> processInstanceIds) {
+        this.processInstanceIds = processInstanceIds;
+    }
+
+    @Override
+    public String toString() {
+        return "BatchQueryRequest{" +
+                "ids=" + ids +
+                '}';
+    }
+
 }
+
