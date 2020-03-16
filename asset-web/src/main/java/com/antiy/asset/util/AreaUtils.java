457f11a03776b178d4ee1af984c92cbaed578a48
liulusheng <liulusheng@antiy.cn>
2020-03-16 17:23:44 +0800

    feat: 增加根据区域id获取所有下级区域id

--- asset-web/src/main/java/com/antiy/asset/util/AreaUtils.java
+++ asset-web/src/main/java/com/antiy/asset/util/AreaUtils.java
@@ -6,6 +6,7 @@ import org.apache.commons.collections.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -20,11 +21,11 @@ public class AreaUtils {
      * @param areaId
      * @return 如果返回null，则说明不存在下级节点
      */
-    private List<String> getNextArea(Integer areaId) {
+    private static List<String> getNextArea(String areaId) {
         List<SysArea> childAreaList = LoginUserUtil.getLoginUser().getAreas();;
         if (null != areaId) {
             childAreaList = LoginUserUtil.getLoginUser().getAreas().stream()
-                .filter(area -> area.getParentId().equals(areaId + "")).collect(Collectors.toList());
+                .filter(area -> area.getParentId().equals(areaId)).collect(Collectors.toList());
         }
 
         if (CollectionUtils.isNotEmpty(childAreaList)) {
@@ -35,5 +36,27 @@ public class AreaUtils {
         result.add(areaId);
 
         return result;
+    }
+
+    /**
+     * 获取某节点所有下级节点，包括该节点本身
+     * @param areaId
+     * @return
+     */
+    public static List<String> getWholeNextArea(String areaId) {
+        Objects.requireNonNull(areaId,"区域id不能为空");
+        List<String> areaIds =getNextArea(areaId);
+        if (areaIds.contains(areaId)){
+            return areaIds;
+        }
+        for (int i = 0; i < areaIds.size(); i++) {
+            List<String> nextAreaIds = getNextArea(areaIds.get(i));
+            if (!nextAreaIds.contains(areaIds.get(i))){
+                areaIds.addAll(nextAreaIds);
+            }
+        }
+        //添加自身节点
+        areaIds.add(areaId);
+        return areaIds;
     }
 }
