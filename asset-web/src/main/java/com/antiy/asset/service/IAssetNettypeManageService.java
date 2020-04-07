40b15b1363033aab64c06546a3b74570676fec27
wangqian <wangqian@antiy.cn>
2020-04-07 14:38:21 +0800

    feat: 网络类型管理

---
+++ asset-web/src/main/java/com/antiy/asset/service/IAssetNettypeManageService.java
@@ -0,0 +1,67 @@
+package com.antiy.asset.service;
+
+import com.antiy.asset.entity.AssetNettypeManage;
+import com.antiy.asset.vo.query.AssetNettypeManageQuery;
+import com.antiy.asset.vo.request.AssetNettypeManageRequest;
+import com.antiy.asset.vo.response.AssetNettypeManageResponse;
+import com.antiy.common.base.BaseRequest;
+import com.antiy.common.base.IBaseService;
+import com.antiy.common.base.PageResult;
+import com.antiy.common.base.QueryCondition;
+
+import java.util.List;
+
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author wangqian
+ * @since 2020-04-07
+ */
+public interface IAssetNettypeManageService extends IBaseService<AssetNettypeManage> {
+
+        /**
+         * 保存
+         * @param request
+         * @return
+         */
+        String saveAssetNettypeManage(AssetNettypeManageRequest request) throws Exception;
+
+        /**
+         * 修改
+         * @param request
+         * @return
+         */
+        String updateAssetNettypeManage(AssetNettypeManageRequest request) throws Exception;
+
+        /**
+         * 查询对象集合
+         * @param query
+         * @return
+         */
+        List<AssetNettypeManageResponse> queryListAssetNettypeManage(AssetNettypeManageQuery query) throws Exception;
+
+        /**
+         * 分页查询
+         * @param query
+         * @return
+         */
+        PageResult<AssetNettypeManageResponse> queryPageAssetNettypeManage(AssetNettypeManageQuery query) throws Exception;
+
+        /**
+         * 通过ID查询
+         * @param queryCondition
+         * @return
+         */
+        AssetNettypeManageResponse queryAssetNettypeManageById(QueryCondition queryCondition) throws Exception;
+
+        /**
+         * 通过ID删除
+         * @param baseRequest
+         * @return
+         */
+        String deleteAssetNettypeManageById(BaseRequest baseRequest) throws Exception;
+
+}
---
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetNettypeManageServiceImpl.java
@@ -0,0 +1,93 @@
+package com.antiy.asset.service.impl;
+
+import com.antiy.asset.dao.AssetNettypeManageDao;
+import com.antiy.asset.entity.AssetNettypeManage;
+import com.antiy.asset.service.IAssetNettypeManageService;
+import com.antiy.asset.vo.query.AssetNettypeManageQuery;
+import com.antiy.asset.vo.request.AssetNettypeManageRequest;
+import com.antiy.asset.vo.response.AssetNettypeManageResponse;
+import com.antiy.common.base.BaseConverter;
+import com.antiy.common.base.BaseRequest;
+import com.antiy.common.base.BaseServiceImpl;
+import com.antiy.common.base.PageResult;
+import com.antiy.common.base.QueryCondition;
+import com.antiy.common.exception.BusinessException;
+import com.antiy.common.utils.LogUtils;
+import com.antiy.common.utils.LoginUserUtil;
+import com.antiy.common.utils.ParamterExceptionUtils;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author wangqian
+ * @since 2020-04-07
+ */
+@Service
+public class AssetNettypeManageServiceImpl extends BaseServiceImpl<AssetNettypeManage> implements IAssetNettypeManageService{
+
+        private Logger logger = LogUtils.get(this.getClass());
+
+        @Resource
+        private AssetNettypeManageDao assetNettypeManageDao;
+        @Resource
+        private BaseConverter<AssetNettypeManageRequest, AssetNettypeManage>  requestConverter;
+        @Resource
+        private BaseConverter<AssetNettypeManage, AssetNettypeManageResponse> responseConverter;
+
+        @Override
+        public String saveAssetNettypeManage(AssetNettypeManageRequest request) throws Exception {
+            AssetNettypeManage assetNettypeManage = requestConverter.convert(request, AssetNettypeManage.class);
+            assetNettypeManage.setCreateUser(LoginUserUtil.getLoginUser().getId());
+            assetNettypeManage.setGmtCreate(System.currentTimeMillis());
+            Integer count = assetNettypeManageDao.getCountByNetTypeName(null, assetNettypeManage.getNetTypeName());
+            if (count > 0) {
+                throw new BusinessException("网络类型名称已存在");
+            }
+            assetNettypeManageDao.insert(assetNettypeManage);
+            return assetNettypeManage.getStringId();
+        }
+
+        @Override
+        public String updateAssetNettypeManage(AssetNettypeManageRequest request) throws Exception {
+            AssetNettypeManage assetNettypeManage = requestConverter.convert(request, AssetNettypeManage.class);
+            Integer count = assetNettypeManageDao.getCountByNetTypeName(assetNettypeManage.getId(), assetNettypeManage.getNetTypeName());
+            if (count > 0) {
+                throw new BusinessException("网络类型名称已存在");
+            }
+            assetNettypeManage.setModifiedUser(LoginUserUtil.getLoginUser().getId());
+            assetNettypeManage.setGmtModified(System.currentTimeMillis());
+            return assetNettypeManageDao.update(assetNettypeManage).toString();
+        }
+
+        @Override
+        public List<AssetNettypeManageResponse> queryListAssetNettypeManage(AssetNettypeManageQuery query) throws Exception {
+            List<AssetNettypeManage> assetNettypeManageList = assetNettypeManageDao.findQuery(query);
+            //TODO
+            return responseConverter.convert(assetNettypeManageList,AssetNettypeManageResponse.class);
+        }
+
+        @Override
+        public PageResult<AssetNettypeManageResponse> queryPageAssetNettypeManage(AssetNettypeManageQuery query) throws Exception {
+                return new PageResult<AssetNettypeManageResponse>(query.getPageSize(), this.findCount(query),query.getCurrentPage(), this.queryListAssetNettypeManage(query));
+        }
+
+        @Override
+        public AssetNettypeManageResponse queryAssetNettypeManageById(QueryCondition queryCondition) throws Exception{
+             ParamterExceptionUtils.isBlank(queryCondition.getPrimaryKey(), "主键Id不能为空");
+             AssetNettypeManageResponse assetNettypeManageResponse = responseConverter
+                .convert(assetNettypeManageDao.getById(queryCondition.getPrimaryKey()), AssetNettypeManageResponse.class);
+             return assetNettypeManageResponse;
+        }
+
+        @Override
+        public String deleteAssetNettypeManageById(BaseRequest baseRequest) throws Exception{
+             ParamterExceptionUtils.isBlank(baseRequest.getStringId(), "主键Id不能为空");
+                return assetNettypeManageDao.deleteById(baseRequest.getStringId()).toString();
+        }
+}
