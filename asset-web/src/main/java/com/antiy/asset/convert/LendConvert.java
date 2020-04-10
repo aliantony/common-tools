050d6fc1c02b5a38a358010d22307a1c2a59d96f
wanghongyu@antiy.cn <wanghongyu@antiy.cn>
2020-04-10 13:56:02 +0800

    feat: 导入人员

---
+++ asset-web/src/main/java/com/antiy/asset/convert/LendConvert.java
@@ -0,0 +1,33 @@
+package com.antiy.asset.convert;
+
+import com.antiy.asset.templet.AssetLendRelationEntity;
+import com.antiy.asset.vo.response.AssetLendRelationResponse;
+import com.antiy.common.base.BaseConverter;
+import com.antiy.common.utils.DateUtils;
+import com.antiy.common.utils.LogUtils;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Component
+public class LendConvert extends BaseConverter<AssetLendRelationResponse, AssetLendRelationEntity> {
+    private final Logger logger = LogUtils.get();
+
+    @Override
+    protected void convert(AssetLendRelationResponse asset, AssetLendRelationEntity assetEntity) {
+        assetEntity.setLendTime(longToDateString(asset.getLendTime()));
+        assetEntity
+            .setLendPeriods(longToDateString(asset.getLendTime()) + "-" + longToDateString(asset.getLendPeriods()));
+        super.convert(asset, assetEntity);
+    }
+
+    private String longToDateString(Long datetime) {
+        if (Objects.nonNull(datetime) && !Objects.equals(datetime, 0L)) {
+            return DateUtils.getDataString(new Date(datetime), DateUtils.WHOLE_FORMAT);
+        }
+        return "";
+    }
+
+}
