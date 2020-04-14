551a970ae09676fc03fcb77498434346b6d3898b
wangqian <wangqian@antiy.cn>
2020-04-14 16:05:22 +0800

    feat: 根据权限标识和区域id查询有权限的人员

--- asset-web/src/main/java/com/antiy/asset/service/IAssetNettypeManageService.java
+++ asset-web/src/main/java/com/antiy/asset/service/IAssetNettypeManageService.java
@@ -69,4 +69,11 @@ public interface IAssetNettypeManageService extends IBaseService<AssetNettypeMan
          * @return
          */
         List<AssetNettypeManageResponse> getAllList() throws Exception;
+
+        /**
+         * 根据网络类型名称查询id
+         * @param name
+         * @return
+         */
+        List<String> getIdsByName(String name);
 }
