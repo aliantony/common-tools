436813dd892ff3f4a946328cda46fe9ccdcde2e0
shenliang <shenliang@antiy.cn>
2020-04-16 14:39:17 +0800

    fix：修改订单提交接口

--- asset-dao/src/main/java/com/antiy/asset/entity/AssetOaOrderLend.java
+++ asset-dao/src/main/java/com/antiy/asset/entity/AssetOaOrderLend.java
@@ -2,6 +2,7 @@ package com.antiy.asset.entity;
 
 
 import com.antiy.common.base.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * <p>出借订单拒绝表</p>
@@ -13,20 +14,31 @@ import com.antiy.common.base.BaseEntity;
 public class AssetOaOrderLend extends BaseEntity {
 
 
-private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-    /**
-    *  订单表编号
-    */
-    private String orderNumber;
-    /**
-    *  是否拒绝出借，1是0否
-    */
+    @ApiModelProperty("是否出借，1是 0否")
     private Integer lendStatus;
-    /**
-    *  拒绝原因 
-    */
+
+    @ApiModelProperty("拒绝原因")
     private String refuseReason;
+
+    @ApiModelProperty("订单流水号")
+    private String orderNumber;
+
+    @ApiModelProperty("借出人")
+    private Integer lendUserId;
+
+    @ApiModelProperty("借出日期")
+    private Long lendTime;
+
+    @ApiModelProperty("归还日期")
+    private Long returnTime;
+
+    @ApiModelProperty("借出说明")
+    private String lendRemark;
+
+    @ApiModelProperty("记录生成时间")
+    private Long gmtCreate;
 
 
 
@@ -35,7 +47,7 @@ private static final long serialVersionUID = 1L;
     }
 
     public void setOrderNumber(String orderNumber) {
-    this.orderNumber = orderNumber;
+        this.orderNumber = orderNumber;
     }
 
 
@@ -44,7 +56,7 @@ private static final long serialVersionUID = 1L;
     }
 
     public void setLendStatus(Integer lendStatus) {
-    this.lendStatus = lendStatus;
+        this.lendStatus = lendStatus;
     }
 
 
@@ -53,16 +65,60 @@ private static final long serialVersionUID = 1L;
     }
 
     public void setRefuseReason(String refuseReason) {
-    this.refuseReason = refuseReason;
+        this.refuseReason = refuseReason;
     }
 
+    public Integer getLendUserId() {
+        return lendUserId;
+    }
+
+    public void setLendUserId(Integer lendUserId) {
+        this.lendUserId = lendUserId;
+    }
+
+    public Long getLendTime() {
+        return lendTime;
+    }
+
+    public void setLendTime(Long lendTime) {
+        this.lendTime = lendTime;
+    }
+
+    public Long getReturnTime() {
+        return returnTime;
+    }
+
+    public void setReturnTime(Long returnTime) {
+        this.returnTime = returnTime;
+    }
+
+    public String getLendRemark() {
+        return lendRemark;
+    }
+
+    public void setLendRemark(String lendRemark) {
+        this.lendRemark = lendRemark;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
 
     @Override
     public String toString() {
-            return "AssetOaOrderLend{" +
-                        ", orderNumber=" + orderNumber +
-                        ", lendStatus=" + lendStatus +
-                        ", refuseReason=" + refuseReason +
-            "}";
+        return "AssetOaOrderLend{" +
+                ", orderNumber=" + orderNumber +
+                ", lendStatus=" + lendStatus +
+                ", refuseReason=" + refuseReason +
+                ", lendUserId=" + lendUserId +
+                ", lendTime=" + lendTime +
+                ", returnTime=" + returnTime +
+                ", lendRemark=" + lendRemark +
+                ", gmtCreate=" + gmtCreate +
+                "}";
     }
 }
