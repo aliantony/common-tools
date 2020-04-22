ee8ab7c1ae7ccd88327c10c9ff5f48e753d57f80
liulusheng <liulusheng@antiy.cn>
2020-04-22 18:00:32 +0800

    feat: 批量准入实施操作日志

---
+++ asset-vo/src/main/java/com/antiy/asset/vo/query/UncodeBaseQuery.java
@@ -0,0 +1,17 @@
+package com.antiy.asset.vo.query;
+
+/**
+ * @author liulusheng
+ * @since 2020/4/22
+ */
+public class UncodeBaseQuery {
+    private String primaryKey;
+
+    public String getPrimaryKey() {
+        return primaryKey;
+    }
+
+    public void setPrimaryKey(String primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+}
