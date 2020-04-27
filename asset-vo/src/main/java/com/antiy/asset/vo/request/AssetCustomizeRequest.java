4f87a8e304096482836331b89c617a365c4bb641
lvliang <lvliang@antiy.cn>
2020-04-27 11:11:03 +0800

    feat: 资产查询

--- asset-vo/src/main/java/com/antiy/asset/vo/request/AssetCustomizeRequest.java
+++ asset-vo/src/main/java/com/antiy/asset/vo/request/AssetCustomizeRequest.java
@@ -1,7 +1,5 @@
 package com.antiy.asset.vo.request;
 
-import com.antiy.common.base.BaseRequest;
-
 /**
  * <p> AssetIpRelationRequest 请求对象 </p>
  *
@@ -9,18 +7,18 @@ import com.antiy.common.base.BaseRequest;
  * @since 2018-12-27
  */
 
-public class AssetCustomizeRequest extends BaseRequest {
+public class AssetCustomizeRequest {
 
-    private String key;
+    private String name;
 
     private String value;
 
-    public String getKey() {
-        return key;
+    public String getName() {
+        return name;
     }
 
-    public void setKey(String key) {
-        this.key = key;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getValue() {
