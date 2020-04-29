38e2e1e232b35ce64d798f45b7cd007fd603677e
lvliang <lvliang@antiy.cn>
2020-04-29 14:10:36 +0800

    feat: 资产变更

--- asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
+++ asset-web/src/main/java/com/antiy/asset/service/impl/AssetServiceImpl.java
@@ -1,5 +1,37 @@
 package com.antiy.asset.service.impl;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.compress.utils.Lists;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.antiy.asset.cache.AssetBaseDataCache;
 import com.antiy.asset.dao.*;
 import com.antiy.asset.entity.*;
@@ -30,36 +62,6 @@ import com.antiy.common.exception.BusinessException;
 import com.antiy.common.exception.RequestParamValidateException;
 import com.antiy.common.utils.*;
 import com.antiy.common.utils.DataTypeUtils;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.MapUtils;
-import org.apache.commons.compress.utils.Lists;
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.OutputStream;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * <p> 资产主表 服务实现类 </p>
@@ -170,7 +172,7 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
     private AssetCpeTreeDao                                                     treeDao;
     private Object                                                              lock     = new Object();
     @Resource
-    private  AssetBusinessRelationDao relationDao;
+    private AssetBusinessRelationDao                                            relationDao;
     @Resource
     private IAssetCpeTreeService                                                assetCpeTreeService;
 
@@ -196,8 +198,11 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
                     SysArea sysArea = redisUtil.getObject(key, SysArea.class);
                     BusinessExceptionUtils.isTrue(!Objects.isNull(sysArea), "当前区域不存在，或已经注销");
                     if (StringUtils.isNotBlank(requestAsset.getResponsibleUserId())) {
-                        BusinessExceptionUtils.isTrue(!Objects.isNull(assetBaseDataCache.get(AssetBaseDataCache.ASSET_USER,
-                                DataTypeUtils.stringToInteger(requestAsset.getResponsibleUserId()))), "使用者不存在，或已经注销");
+                        BusinessExceptionUtils
+                            .isTrue(
+                                !Objects.isNull(assetBaseDataCache.get(AssetBaseDataCache.ASSET_USER,
+                                    DataTypeUtils.stringToInteger(requestAsset.getResponsibleUserId()))),
+                                "使用者不存在，或已经注销");
                     }
                     List<AssetGroupRequest> assetGroup = requestAsset.getAssetGroups();
                     Asset asset = requestConverter.convert(requestAsset, Asset.class);
@@ -1387,11 +1392,11 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
             return true;
         }
         // 比较软件
-        if (!oldSoft.containsAll(newSoft) || !newSoft.containsAll(oldSoft)) {
+        if (!(oldSoft.containsAll(newSoft) && newSoft.containsAll(oldSoft))) {
             return true;
         }
         // 比较硬盘
-        if (!newDisk.containsAll(oldDisk) || !oldDisk.containsAll(newDisk)) {
+        if (!(newDisk.containsAll(oldDisk) && !oldDisk.containsAll(newDisk))) {
             return true;
         }
         return false;
@@ -3402,13 +3407,12 @@ public class AssetServiceImpl extends BaseServiceImpl<Asset> implements IAssetSe
             activityHandleRequests.add(activityHandleRequest);
         }
 
-        //删除关联业务的资产
+        // 删除关联业务的资产
 
         relationDao.deleteByAssetId(assetIdList);
 
         if (activityHandleRequests.size() > 0)
             activityClient.completeTaskBatch(activityHandleRequests);
-
 
         LogUtils.info(logger, AssetEventEnum.NO_REGISTER.getName() + " {}", list);
         return currentAssetList.size();
