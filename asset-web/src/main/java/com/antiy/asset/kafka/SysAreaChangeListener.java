61c2df27621ce2d4068d40f8aca068c213ac6754
lvliang <lvliang@antiy.cn>
2020-05-07 11:09:34 +0800

    feat: 资产查询

--- asset-web/src/main/java/com/antiy/asset/kafka/SysAreaChangeListener.java
+++ asset-web/src/main/java/com/antiy/asset/kafka/SysAreaChangeListener.java
@@ -5,13 +5,10 @@ import java.util.Objects;
 
 import javax.annotation.Resource;
 
-import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSONObject;
@@ -26,13 +23,11 @@ import com.antiy.common.utils.LogUtils;
 public class SysAreaChangeListener {
     @Resource
     private AssetDao assetDao;
-
     private Logger   log = LogUtils.get(SysAreaChangeListener.class);
 
-    //@KafkaListener(topics = "${kafka.sysarea.change.topic}")
-    public void sysAreaChange(ConsumerRecord<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
-                              Consumer consumer, Acknowledgment ack) {
-        Map<String, Integer> area = JSONObject.parseObject((String) record.value(), Map.class);
+    @KafkaListener(topics = "#{'${kafka.sysarea.change.topic}'}", containerFactory = "sampleListenerContainerFactory")
+    public void sysAreaChange(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        Map<String, Integer> area = JSONObject.parseObject(record.value(), Map.class);
         if (!Objects.isNull(area.get("childId")) && !Objects.isNull(area.get("parentId"))) {
             log.info("更新资产区域");
             assetDao.updateAreaIdOfAsset(area);
