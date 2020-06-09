c3651b809effd3b957cdc76f9ed00c85ebdf1a1e
wangqian <wangqian@antiy.cn>
2020-06-09 15:45:28 +0800

    Initial commit

---
+++ common-tools/src/main/java/com/boot/Application.java
@@ -0,0 +1,18 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program common-tools
+ * @description 
+ * @author antony
+ * created on 2020-06-09
+ * @version  1.0.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ common-tools/src/main/java/com/boot/web/SystemController.java
@@ -0,0 +1,40 @@
+package com.boot.web;
+
+import com.boot.annotation.ResponseResult;
+import com.boot.common.SystemHardwareInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program common-tools
+ * @description 
+ * @author wangqian
+ * created on 2020-06-09
+ * @version  1.0.0
+ */
+@Api(value = "系统信息api", description ="系统信息api")
+@RestController
+@ResponseResult
+public class SystemController {
+
+    /**
+     * 系统硬件信息页面
+     *
+     * @author antony
+     * @Date 2018/12/24 22:43
+     */
+    @ApiOperation("获取系统信息")
+    @GetMapping("/systemInfo")
+    public SystemHardwareInfo systemInfo() {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        SystemHardwareInfo systemHardwareInfo = new SystemHardwareInfo();
+        systemHardwareInfo.copyTo();
+        watch.stop();
+        System.out.println(watch.getTotalTimeMillis());
+        return systemHardwareInfo;
+    }
+}
